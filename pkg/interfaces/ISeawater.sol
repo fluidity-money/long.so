048bbeb40d9b037a80d7aea9f101e704d4433100
user <alex@iometelabs.io>
2023-09-06 20:36:00 +0900

    Add interfaces for seawater and the token LP amount tracking

---
+++ pkg/interfaces/ISeawater.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+pragma abicoder v2;
+
+import "./IERC1155.sol";
+
+interface ISeawater is IERC1155 {
+    function swap1In(uint8 poolId) external returns (uint256 returned);
+    function swap2In(uint8 poolId1, uint8 poolId2) external returns (uint256 returned);
+
+    function swap1Out(uint8 poolId) external returns (uint256 returned);
+    function swap2Out(uint8 poolId) external returns (uint256 returned);
+
+    function burn(uint8 poolId) external returns (uint112 burned);
+    function mint(uint8 poolId) external returns (uint112 minted);
+
+    /// @notice implements EIP1155
+    function balanceOfUnderlying(uint8 poolId, address spender) external returns (uint256 amount);
+}
