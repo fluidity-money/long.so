0e8e65884d0e302c2d8764b650d3e70375873630
user <alex@iometelabs.io>
2023-09-18 10:12:54 +0900

    Add ERC721 mock and mock contract

--- pkg/interfaces/ISeawaterOwnership.sol
+++ pkg/interfaces/ISeawaterOwnership.sol
@@ -5,9 +5,7 @@ pragma solidity 0.8.16;
 interface ISeawaterOwnership {
     function ownerOf(uint256 _tokenId) external view returns (address);
 
-    function transfer(
-        uint256 _tokenId,
-        address _recipient,
-        uint256 _units
-    ) external view returns (uint256 newTokenId);
+    function transfer(uint256 _tokenId, address _recipient) external;
+
+    function balanceOf(address _spender) external view returns (uint256);
 }
