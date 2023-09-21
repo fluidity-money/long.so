c431920f584628a0e32894bb133a069375f49e1e
elle <elle@catgirl.software>
2023-09-21 14:21:52 +0930

    comment

--- pkg/contracts/seawater/src/main.rs
+++ pkg/contracts/seawater/src/main.rs
@@ -4,4 +4,6 @@ pub extern "C" fn user_entrypoint(len: usize) -> usize {
     stylus_entrypoint(len)
 }
 
+// for whatever reason, even with `#[no_main]` or `#[cfg(test)] fn main(){}` this crate fails to
+// build for tests without this
 fn main() {}
