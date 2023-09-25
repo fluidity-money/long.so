b0a7d909eb6e8d3944a5d44d46738d47e52ccb7d
elle <elle@catgirl.software>
2023-09-25 17:44:54 +0930

    start of diamond

---
+++ pkg/interfaces/ISeawaterExecutors.sol
@@ -0,0 +1,63 @@
+// SPDX-Identifier: MIT
+pragma solidity 0.8.16;
+
+interface ISeawaterExecutorSwap {
+    function swap(
+        address pool,
+        bool zeroForOne,
+        int256 amount,
+        uint256 priceLimit
+    ) external returns (int256, int256);
+
+    function swap2ExactIn(
+        address from,
+        address to,
+        uint256 amount,
+        uint256 minOut
+    ) external returns (uint256);
+
+    function swap2ExactOut(
+        address from,
+        address to,
+        uint256 amount,
+        uint256 maxIn
+    ) external returns (uint256);
+}
+
+interface ISeawaterExecutorPosition {
+    function updatePosition(
+        address pool,
+        address owner,
+        int32 lower,
+        int32 upper,
+        int128 delta
+    ) external returns (int256, int256);
+
+    function collect(
+        address pool,
+        address owner,
+        int32 lower,
+        int32 upper,
+        uint256 amount0,
+        uint256 u128
+    ) external returns (uint128, uint128);
+}
+
+interface ISeawaterExecutorAdmin {
+    function init(
+        address pool,
+        uint256 sqrtPriceX96,
+        uint32 fee,
+        uint8 tickSpacing
+    ) external;
+
+    function collectProtocol(
+        address pool,
+        uint128 amount0,
+        uint128 amount1
+    ) external returns (uint128, uint128);
+}
+
+interface ISeawaterExecutorFallback {
+
+}
