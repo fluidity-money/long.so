a50a24259543d12d6cbac063e384b6a21457f03c
eli-d <64763513+eli-d@users.noreply.github.com>
2023-10-12 16:28:36 +1030

    semifunction test for `fn swap`

--- pkg/ethers-tests/test.ts
+++ pkg/ethers-tests/test.ts
@@ -1,4 +1,4 @@
-import {Contract, ContractFactory, JsonRpcProvider, Wallet } from "ethers";
+import {Contract, ContractFactory, JsonRpcProvider, Log, MaxUint256, Wallet } from "ethers";
 import LightweightERC20 from "../out/LightweightERC20.sol/LightweightERC20.json"
 import {abi as SeawaterABI}  from "../out/SeawaterAMM.sol/SeawaterAMM.json"
 import test from "node:test"
@@ -10,7 +10,8 @@ test("amm", async t => {
     const provider = new JsonRpcProvider(RPC_URL)
     const signer = new Wallet("0xb6b15c8cb491557369f3c7d2c287b053eb229daa9c22138887752191c9520659", provider)
 
-    const amm = new Contract("0x4CeF6f83aD0176094a2Cf9dF04Edc8B730600E44", SeawaterABI, signer);
+    // FIXME needs to be manually set
+    const amm = new Contract("0xCA73cf68a91E7173d4A989A02227d9721519e585", SeawaterABI, signer);
     const erc20Factory = new ContractFactory(LightweightERC20.abi, LightweightERC20.bytecode, signer)
 
     const erc20ContractTusdc = await erc20Factory.deploy("Test USDC", "TUSDC", 6, 1_000_000*1_000_000, defaultAccount);
@@ -31,39 +32,78 @@ test("amm", async t => {
     // uint32 fee,
     // uint8 tickSpacing,
     // uint128 maxLiquidityPerTick
-    let response = await amm.init(fusdcAddress, 1,0,2,10000000000);
-    console.log("init result",response)
+    let response = await amm.init(tusdcAddress, BigInt("792281625142643375935439503360"),0,1,100000000000);
+    await response.wait();
+    // approve amm for both contracts
+    // initialise an empty position
+    // update the position with liquidity
+    // then make a swap
+    await (await fusdcContract.approve(await amm.getAddress(), MaxUint256)).wait()
+    await (await tusdcContract.approve(await amm.getAddress(), MaxUint256)).wait()
+
+    const mintResult = await amm.mintPosition(tusdcAddress, BigInt("39122"), BigInt("50108"))
+    const [mintLog]: [mintLog: Log] = await mintResult.wait()
+    type mintEventArgs = [
+        BigInt,
+        string,
+        string,
+        BigInt,
+        BigInt,
+    ]
+
+    // has an issue with readonly typing
+    // @ts-ignore
+    const {args}  = amm.interface.parseLog(mintLog) || {}
+    const [id, _, pool, low, high] = args as unknown as mintEventArgs
+
+    // @ts-ignore
+    console.log("pool",pool,"id",id,"delta",high - low)
+
+    const updatePositionResult = await amm.updatePosition(tusdcAddress, id, 200)
+    await updatePositionResult.wait()
+    const receipt = await provider.getTransactionReceipt(updatePositionResult.hash)
+    console.log("updatePosition logs",receipt?.logs)
 
     await t.test("swap in amounts correct", async _ => {
         const fusdcBeforeBalance = await fusdcContract.balanceOf(defaultAccount)
         const tusdcBeforeBalance = await tusdcContract.balanceOf(defaultAccount)
 
-        response = await amm.swapIn(tusdcAddress, 10, 0)
-        console.log("result",response)
+        // TODO test on swapIn, not raw swap
+        // address _token,
+        // bool _zeroForOne,
+        // int256 _amount,
+        // uint256 _priceLimitX96
+        response = await amm.swap(tusdcAddress, true, 10000, BigInt("0x0000000000000000000000000000000000000007bef7ac53d3b66c5eba01e41f"))
+        await response.wait();
+
+        console.log("swap logs",(await provider.getTransactionReceipt(response.hash))?.logs)
 
         const fusdcAfterBalance = await fusdcContract.balanceOf(defaultAccount)
         const tusdcAfterBalance = await tusdcContract.balanceOf(defaultAccount)
-        const expectedFusdcAfterBalance = fusdcBeforeBalance - BigInt(10);
-        const expectedTusdcAfterBalance = tusdcBeforeBalance - BigInt(10);
+        const expectedFusdcAfterBalance = fusdcBeforeBalance - BigInt(1000);
+        const expectedTusdcAfterBalance = tusdcBeforeBalance - BigInt(1000);
+
+        console.log("fusdc after",fusdcAfterBalance)
+        console.log("tusdc after",tusdcAfterBalance)
 
         assert(fusdcAfterBalance === expectedFusdcAfterBalance, `expected balances to match! got: ${fusdcAfterBalance}, expected ${expectedFusdcAfterBalance}`)
         assert(tusdcAfterBalance === expectedTusdcAfterBalance, `expected balances to match! got: ${tusdcAfterBalance}, expected ${expectedTusdcAfterBalance}`)
 
     })
-    await t.test("swap out amounts correct", async _ => {
-        const fusdcBeforeBalance = await fusdcContract.balanceOf(defaultAccount)
-        const tusdcBeforeBalance = await tusdcContract.balanceOf(defaultAccount)
+    // await t.test("swap out amounts correct", async _ => {
+    //     const fusdcBeforeBalance = await fusdcContract.balanceOf(defaultAccount)
+    //     const tusdcBeforeBalance = await tusdcContract.balanceOf(defaultAccount)
 
-        response = await amm.swapOut(tusdcAddress, 10, 0)
+    //     response = await amm.swapOut(tusdcAddress, 10, 0)
 
-        const fusdcAfterBalance = await fusdcContract.balanceOf(defaultAccount)
-        const tusdcAfterBalance = await tusdcContract.balanceOf(defaultAccount)
-        const expectedFusdcAfterBalance = fusdcBeforeBalance - BigInt(10);
-        const expectedTusdcAfterBalance = tusdcBeforeBalance - BigInt(10);
+    //     const fusdcAfterBalance = await fusdcContract.balanceOf(defaultAccount)
+    //     const tusdcAfterBalance = await tusdcContract.balanceOf(defaultAccount)
+    //     const expectedFusdcAfterBalance = fusdcBeforeBalance - BigInt(10);
+    //     const expectedTusdcAfterBalance = tusdcBeforeBalance - BigInt(10);
 
-        assert(fusdcAfterBalance === expectedFusdcAfterBalance, `expected balances to match! got: ${fusdcAfterBalance}, expected ${expectedFusdcAfterBalance}`)
-        assert(tusdcAfterBalance === expectedTusdcAfterBalance, `expected balances to match! got: ${tusdcAfterBalance}, expected ${expectedTusdcAfterBalance}`)
-    })
+    //     assert(fusdcAfterBalance === expectedFusdcAfterBalance, `expected balances to match! got: ${fusdcAfterBalance}, expected ${expectedFusdcAfterBalance}`)
+    //     assert(tusdcAfterBalance === expectedTusdcAfterBalance, `expected balances to match! got: ${tusdcAfterBalance}, expected ${expectedTusdcAfterBalance}`)
+    // })
 
     await t.test("raw swap", async t => {
         t.todo();
