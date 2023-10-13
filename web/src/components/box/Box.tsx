cd5f02bfa6750248fa9939ce2fffff8b1b1ea80c
Alfie Edgeworth <0xalfie@gmail.com>
2023-10-13 14:01:38 +1030

    update menu, chip, box components

--- web/src/components/box/Box.tsx
+++ web/src/components/box/Box.tsx
@@ -2,16 +2,26 @@ import { motion } from 'framer-motion'
 import styles from './Box.module.scss'
 
 interface IBox {
-  children: React.ReactNode
+  children?: React.ReactNode
   layoutId?: string
+  background?: 'light' | 'dark'
+  className?: string
 }
 
 const Box: React.FC<IBox> = (props) => {
-  const { children, layoutId } = props
+  const { children, layoutId, background = 'light', className = '' } = props
+
+  const classes = `
+    ${styles.Box}
+    ${styles[background]}
+    ${className}
+  `
+
   return (
     <motion.div
       layoutId={layoutId}
-      className={styles.Box}
+      className={classes}
+      transition={{ duration: 0.2, ease: 'easeInOut' }}
     >
       {children}
     </motion.div>
