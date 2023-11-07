664507ba9a4127f2cc09b233ebd4917f3aa3533c
Alfie Edgeworth <0xalfie@gmail.com>
2023-11-07 22:00:46 +1030

    combined stash

---
+++ web/src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,9 @@
+interface IDropdown {
+  children: React.ReactNode
+}
+
+const Dropdown: React.FC<IDropdown> = (props) => {
+  return <></>
+}
+
+export { Dropdown }
---
+++ web/src/components/Token/Token.tsx
@@ -0,0 +1,8 @@
+import USDC from '@/assets/icons/USDC.svg'
+import Image from 'next/image'
+
+const Token = () => {
+  return <Image alt="test" height="32" width="32" src={USDC} />
+}
+
+export { Token }
