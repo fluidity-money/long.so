44d68d61d515f22ddfd2b64663175a29f8467342
Alfie Edgeworth <0xalfie@gmail.com>
2023-11-08 13:20:02 +1030

    updated ani details for tokens

--- web/src/components/Token/Token.tsx
+++ web/src/components/Token/Token.tsx
@@ -2,7 +2,7 @@ import USDC from '@/assets/icons/USDC.svg'
 import ETH from '@/assets/icons/ETH.svg'
 import Image from 'next/image'
 import styles from './Token.module.scss'
-import { motion, useMotionValue } from 'framer-motion'
+import { motion } from 'framer-motion'
 import { useState } from 'react'
 
 const tokenVariants = {
@@ -13,22 +13,22 @@ const tokenVariants = {
       ease: 'easeInOut',
     }
   },
-  hover: {
+  hover: (size: number) => ({
     rotateY: [0, 360],
-    x: [0,4,0,-4,0],
+    x: [0,0+(size*2),0,0-(size*2),0],
     transition: {
-      duration: 2,
+      duration: (0.5 * size) + 1.5,
       ease: 'linear',
       repeat: Infinity,
     }
-  },
-  bounce: {
-    y: [0, -10, 0],
+  }),
+  bounce: (size: number) => ({
+    y: [0, -(2*size + 8), 0],
     transition: {
-      duration: 0.5,
+      duration: 0.3 + (size*0.2),
       ease: 'easeInOut',
     }
-  }
+  })
 }
 
 interface IToken {
@@ -44,8 +44,11 @@ const Token: React.FC<IToken> = (props) => {
     ${styles[size]}
   `
 
+  const sz = size === 'small' ? 0 : size === 'medium' ? 1 : 2
+
   return (
     <motion.div
+      custom={sz}
       className={classNames}
       variants={tokenVariants}
       onMouseEnter={() => setHovered(true)}
@@ -54,6 +57,7 @@ const Token: React.FC<IToken> = (props) => {
     >
 
     <motion.div className={styles.Token}
+      custom={sz}
       variants={tokenVariants}
       initial={'idle'}
       animate={hovered ? 'hover' : 'idle'}
