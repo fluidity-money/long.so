a728a4e3d439ac7ca0a4d5752ac0714985d02847
eli-d <64763513+eli-d@users.noreply.github.com>
2023-11-09 16:47:25 +1030

    begin to support updating/removing positions

--- web/src/util/hooks/useSwap.ts
+++ web/src/util/hooks/useSwap.ts
@@ -5,29 +5,58 @@ import {ActiveTokenContext} from "../context/ActiveTokenContext";
 
 interface UseSwap {
     /**
-     * @description - swapOut fluid -> non-fluid | swapIn non-fluid -> fluidd
-     * @param token - the address of the non-fluid token
-     * @param amountIn - the raw amount of the fluid token to use as input
+     * @description - swap a token for a fluid token, or vice versa
+     * @param amountIn - the raw amount of the token to use as input
      * @param minOut - the minimum output amount, reverting if not reached
+     * @param direction - in: non-fluid -> fluid | out: fluid -> non-fluid
      */
-    swap: (amountIn: string, minOut: string, direction: 'in' | 'out') => Promise<void>
+    swap:  (amountIn: string, minOut: string, direction: 'in' | 'out') => Promise<void>
+    /**
+     * @description - swap two non-fluid tokens
+     * @param amountIn - the raw amount of the token to use as input
+     * @param minOut - the minimum output amount, reverting if not reached
+     * @param direction - in: non-fluid -> fluid | out: fluid -> non-fluid
+     */
+    swap2: (amountIn: string, minOut: string, direction: 'in' | 'out') => Promise<void>
 }
 
 const useSwap = (): UseSwap => {
-    const {activeToken, ammAddress} = useContext(ActiveTokenContext);
+    // use token0 for swaps involving a fluid token
+    const {token0, token1, ammAddress} = useContext(ActiveTokenContext);
 
-    const swap = async (amountIn: string, minOut: string, direction: 'in' | 'out') => {
+    const swap = async(amountIn: string, minOut: string, direction: 'in' | 'out') => {
         const {request: swapRequest} = await prepareWriteContract({
             address: ammAddress,
             abi: SeawaterABI,
             functionName: direction === 'in' ? 'swapIn' : 'swapOut',
-            args: [activeToken, amountIn, minOut]
+            args: [token0, amountIn, minOut]
+        })
+        await writeContract(swapRequest)
+    }
+
+    const swap2 = async(amountIn: string, minOut: string, direction: 'in' | 'out') => {
+        let tokenFrom: string;
+        let tokenTo: string;
+        if (direction === 'in') {
+            tokenFrom = token0;
+            tokenTo = token1;
+        } else {
+            tokenFrom = token0;
+            tokenTo = token1;
+        }
+
+        const {request: swapRequest} = await prepareWriteContract({
+            address: ammAddress,
+            abi: SeawaterABI,
+            functionName: 'swap2ExactIn',
+            args: [tokenFrom, tokenTo, amountIn, minOut]
         })
         await writeContract(swapRequest)
     }
 
     return {
         swap,
+        swap2,
     }
 }
 
