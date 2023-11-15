4e288e13ffaac6af73fba9b569050be88b9613c3
eli-d <64763513+eli-d@users.noreply.github.com>
2023-11-15 15:20:18 +1030

    strongly type `ZeroAddress` constant

--- web/src/util/chainUtils.ts
+++ web/src/util/chainUtils.ts
@@ -2,7 +2,8 @@ import {Hash} from "viem"
 
 const IsHash = (hash?: string): hash is Hash => hash?.startsWith('0x') || false
 
-const ZeroAddress = '0x0000000000000000000000000000000000000000';
+const ZeroAddress: Hash = '0x0000000000000000000000000000000000000000';
+
 
 export {
     IsHash,
