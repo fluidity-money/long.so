4c6323348981bf4fdc45383805c2a9599642118a
eli-d <64763513+eli-d@users.noreply.github.com>
2023-11-21 15:17:00 +1030

    update types in `useSwap`

--- web/src/util/hooks/useSwap.ts
+++ web/src/util/hooks/useSwap.ts
@@ -1,4 +1,4 @@
-import {GetContractResult, prepareWriteContract, writeContract} from "wagmi/actions"
+import {prepareWriteContract, writeContract} from "wagmi/actions"
 import {useContext, useEffect, useMemo, useState} from "react"
 import SeawaterABI from "../abi/SeawaterAMM"
 import {ActiveTokenContext} from "../context/ActiveTokenContext"
@@ -15,6 +15,35 @@ type SeawaterResult<T extends string> =
         >
     >>['result']
 
+// the strongly typed request value of the Seawater contract function `T`
+type SeawaterRequest<T extends string> =
+    Awaited<ReturnType<
+        typeof prepareWriteContract<
+            typeof SeawaterABI, T, number
+        >
+    >>['request']
+
+// the arguments for a given `SeawaterRequest<T>`
+type SeawaterRequestArgs<T extends string> = 
+    'args' extends keyof SeawaterRequest<T> 
+        ? SeawaterRequest<T>['args'] 
+        : never
+
+// the possible function/argument combinations for swaps
+type PrepareContractState = 
+{
+    functionName: 'swapIn',
+    args: SeawaterRequestArgs<'swapIn'>
+} | 
+{
+    functionName: 'swapOut',
+    args: SeawaterRequestArgs<'swapOut'>
+} | 
+{
+    functionName: 'swap2ExactIn',
+    args: SeawaterRequestArgs<'swap2ExactIn'>
+}
+
 interface UseSwapProps {
     amountIn: bigint | string
     minOut: bigint | string
@@ -25,7 +54,7 @@ interface UseSwapProps {
  */
 type UseSwap = ({amountIn, minOut}: UseSwapProps) => {
     /**
-     * @description - swap a token for another token. Uses `token0` and `token1` from `ActiveTokenContext`, and `amountIn` and `minOut` from `UseSwapProps`. If one of the tokens is the fluid token, `swapIn` or `swapOut` are called, otherwise `swap2ExactIn` is called.
+     * @description - swap a token for another token by executing the simulated contract call. Uses `token0` and `token1` from `ActiveTokenContext`, and `amountIn` and `minOut` from `UseSwapProps`. If one of the tokens is the fluid token, `swapIn` or `swapOut` are called, otherwise `swap2ExactIn` is called.
      * @example amount 100 fUSDC -> token0 calls swapOut - swap n token0 for 100 fusdc, result is [n, 100]
      * @example amount 100 token0 -> fUSDC calls swapIn  - swap 100 token0 for n fUSDC, result is [100, n]
      * @example amount 100 token0 -> token1 calls swap2ExactIn - swap 100 token0 for n token1, result is [100, n]
@@ -46,26 +75,30 @@ type UseSwap = ({amountIn, minOut}: UseSwapProps) => {
 const useSwap: UseSwap = ({amountIn, minOut}) => {
     // TODO add approve step
     const {token0, token1, ammAddress} = useContext(ActiveTokenContext)
-    const [functionName, setFunctionName] = useState<'swapIn' | 'swapOut' | 'swap2ExactIn'>('swapIn')
-    // TODO type this
-    const [args, setArgs] = useState<unknown>([])
+
+    const [prepareContractState, setPrepareContractState] = useState<PrepareContractState | undefined>()
 
     // debounce params passed to Wagmi hook to avoid RPC spam
-    const debouncedFunctionName = useDebounce(functionName, 500)
-    const debouncedArgs = useDebounce(args, 500)
+    const debouncedState = useDebounce(prepareContractState, 500)
 
     // on update, set function name and arguments to trigger `usePrepareContractWrite`
     useEffect(() => {
         try {
             if (token0 === FluidTokenAddress) {
-                setFunctionName('swapOut')
-                setArgs([token0, BigInt(amountIn), BigInt(minOut)])
+                setPrepareContractState({
+                    functionName: 'swapOut',
+                    args: [token0, BigInt(amountIn), BigInt(minOut)]
+                })
             } else if (token1 === FluidTokenAddress) {
-                setFunctionName('swapIn')
-                setArgs([token0, BigInt(amountIn), BigInt(minOut)])
-            } else {
-                setFunctionName('swap2ExactIn')
-                setArgs([token0, token1, BigInt(amountIn), BigInt(minOut)])
+                setPrepareContractState({
+                    functionName: 'swapIn',
+                    args: [token0, BigInt(amountIn), BigInt(minOut)]
+                })
+            }  else {
+                setPrepareContractState({
+                    functionName: 'swap2ExactIn', 
+                    args: [token0, token1, BigInt(amountIn), BigInt(minOut)]
+                })
             }
         // ignore string -> BigInt conversion errors
         } catch(e) {}
@@ -75,8 +108,11 @@ const useSwap: UseSwap = ({amountIn, minOut}) => {
     const {config, error} = usePrepareContractWrite({
         address: ammAddress,
         abi: SeawaterABI,
-        functionName: debouncedFunctionName,
-        args: debouncedArgs,
+        functionName: debouncedState?.functionName,
+        // Typescript doesn't support strongly typing this with destructuring
+        // https://github.com/microsoft/TypeScript/issues/46680
+        // @ts-expect-error
+        args: debouncedState?.args,
     })
 
     const result = useMemo(() =>
