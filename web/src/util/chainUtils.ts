d99201629c65c11c962e9a7ec81568c59a70b9e8
eli-d <64763513+eli-d@users.noreply.github.com>
2023-11-21 16:53:29 +1030

    use existing `isHash` from viem

--- web/src/util/chainUtils.ts
+++ web/src/util/chainUtils.ts
@@ -1,11 +1,7 @@
 import {Hash} from "viem"
 
-const IsHash = (hash?: string): hash is Hash => hash?.startsWith('0x') || false
-
 const ZeroAddress: Hash = '0x0000000000000000000000000000000000000000';
 
-
 export {
-    IsHash,
     ZeroAddress,
 }
