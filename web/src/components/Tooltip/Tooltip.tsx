2ba326ddad141617c41c643bb3516bd49374390f
Alfie Edgeworth <0xalfie@gmail.com>
2023-11-27 15:42:52 +1030

    move hoverable logic

--- web/src/components/Tooltip/Tooltip.tsx
+++ web/src/components/Tooltip/Tooltip.tsx
@@ -1,29 +1,12 @@
-import { HTMLAttributes } from 'react'
 import styles from './Tooltip.module.scss'
 
-export interface ITooltip extends HTMLAttributes<HTMLElement> {
-  background?: 'light' | 'dark';
-  cursor?: boolean
+interface ITooltip {
+
 }
 
 const Tooltip: React.FC<ITooltip> = (props) => {
-  const { className = '', style={}, background = 'light', children } = props
-
-  const tooltipClasses = `
-    ${className}
-    ${styles.Tooltip}
-    ${styles[background]}
-  `
-
-  return (
-    <div
-      className={tooltipClasses}
-      style={style}
-      {...props}
-    >
-      {children}
-    </div>
-  )
+  return <div>
+  </div>
 }
 
 export { Tooltip }
