07ad6208acd18ee23c70a2c184911b750a83dc1e
Alfie Edgeworth <0xalfie@gmail.com>
2023-11-27 15:40:14 +1030

    update box sizings and props

--- web/src/components/Box/Box.tsx
+++ web/src/components/Box/Box.tsx
@@ -1,29 +1,59 @@
-import { motion } from 'framer-motion'
+import { TargetAndTransition, VariantLabels, motion } from 'framer-motion'
 import styles from './Box.module.scss'
 
 interface IBox {
-  children?: React.ReactNode;
-  layoutId?: string;
-  background?: 'light' | 'dark';
-  className?: string;
-  pill?: boolean;
+  children?: React.ReactNode
+  layoutId?: string
+  background?: 'light' | 'dark'
+  size?: 'small' | 'medium' | 'large'
+  className?: string
+  pill?: boolean
+  outline?: boolean
+  whileHover?: VariantLabels | TargetAndTransition
+  animate?: VariantLabels | TargetAndTransition
+  initial?: VariantLabels | boolean | TargetAndTransition
+  whileTap?: VariantLabels | TargetAndTransition
+  style?: React.CSSProperties
+  onClick?: () => void
 }
 
 const Box: React.FC<IBox> = (props) => {
-  const { children, layoutId, background = 'light', className = '', pill=false } = props
+  const {
+    children,
+    layoutId,
+    background = 'light',
+    size='medium',
+    className = '',
+    pill = false,
+    outline = false,
+    whileHover,
+    animate,
+    initial,
+    whileTap,
+    style = {},
+    onClick = () => {},
+  } = props
 
   const classes = `
     ${styles.Box}
     ${styles[background]}
+    ${styles[size]}
     ${pill ? styles.pill : ''}
+    ${outline ? styles.outline : ''}
     ${className}
   `
 
   return (
     <motion.div
+      style={style}
       layoutId={layoutId}
       className={classes}
       transition={{ duration: 0.6, type: 'spring', bounce: 0.3 }}
+      whileHover={whileHover}
+      animate={animate}
+      initial={initial}
+      whileTap={whileTap}
+      onClick={onClick}
     >
       {children}
     </motion.div>
