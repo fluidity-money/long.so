41877918d419d58dc45a06cb77227551c3e420c6
Alfie Edgeworth <0xalfie@gmail.com>
2023-11-27 15:40:50 +1030

    flesh out button

--- web/src/components/Button/Button.tsx
+++ web/src/components/Button/Button.tsx
@@ -3,19 +3,37 @@ import styles from './Button.module.scss'
 
 interface IButton {
   children: React.ReactNode;
+  color?: 'light' | 'dark'
+  size?: 'small' | 'medium' | 'large'
+  inline?: boolean
+  onClick?: () => void
 }
 
 const Button: React.FC<IButton> = (props) => {
-  const { children } = props
+  const {
+    children,
+    color = 'dark',
+    size = 'medium',
+    inline = false,
+    onClick = () => { return }
+  } = props
+
+  const classes = `
+    ${styles.Button}
+    ${styles[color]}
+    ${styles[size]}
+    ${inline ? styles.inline : ''}
+  `
+
   return (
     <motion.button
-      className={styles.Button}
+      className={classes}
       style={{
         rotate: 0,
         scale: 1,
       }}
       whileHover={{
-        scale: 1.05,
+        scale: 1.02,
         transition: {
           type: 'spring',
           stiffness: 400,
@@ -23,8 +41,9 @@ const Button: React.FC<IButton> = (props) => {
         },
       }}
       whileTap={{
-        scale: 0.95,
+        scale: 0.98,
       }}
+      onClick={onClick}
     >
       {children}
     </motion.button>
