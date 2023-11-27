10f62ddb1c335184f68bb1a504a13c2600be74eb
Alfie Edgeworth <0xalfie@gmail.com>
2023-11-27 15:38:10 +1030

    add dropdown

--- web/src/components/Dropdown/Dropdown.tsx
+++ web/src/components/Dropdown/Dropdown.tsx
@@ -1,9 +1,64 @@
+import { ReactElement, ReactNode, useState } from 'react'
+import styles from './Dropdown.module.scss'
+import { Hoverable, Tooltip } from '@/components'
+import { motion } from 'framer-motion'
+
+interface IItem {
+  children: ReactNode
+  id: string
+}
+
+const Item: React.FC<IItem> = (props) => {
+  const { children } = props
+  return <div>
+    {children}
+  </div>
+}
+
 interface IDropdown {
-  children: React.ReactNode
+  children: ReactElement<IItem>[]
+  id: string
+  label?: ReactNode
+  initial: string
 }
 
 const Dropdown: React.FC<IDropdown> = (props) => {
-  return <></>
+
+  const { children, id, label, initial } = props
+
+  const dropdownClasses = `
+    ${styles.Dropdown}
+  `
+
+  const [selectedItem, setSelectedItem] = useState(initial)
+
+  return (
+    <div className={dropdownClasses}>
+      {label}
+      <Hoverable
+        tooltip={
+          <Tooltip
+            layoutId={id}
+          >
+            {children}
+          </Tooltip>
+        }
+      >
+        <motion.div className={styles.selected} layoutId={id}>
+        {
+          children.find((child) => child.props.id === selectedItem) || 'test'
+        }
+        </motion.div>
+      </Hoverable>
+      {/* take a list of ReactElement<IItem> to render inside a tooltip */}
+      {/* show tooltip conditionally based on dropdown element state */}
+      {/* keep selected state uncontrolled in dropdown element */}
+      {/* run a find (memo?) and render selected <Item> inline
+        {/* use Hoverable? with shared layoutId and custom positioning */}
+    </div>
+  )
 }
 
-export { Dropdown }
+export default Object.assign(Dropdown, {
+  Item
+})
