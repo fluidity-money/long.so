776c19c10667554bef3dc858db5e933e96cbdb4a
Alfie Edgeworth <0xalfie@gmail.com>
2023-11-27 15:42:17 +1030

    add input component

---
+++ web/src/components/Input/Input.tsx
@@ -0,0 +1,36 @@
+import styles from './Input.module.scss'
+
+export interface ControlledInput extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "onChange" | "value"> {
+  value: string
+  onChange: (s: string) => void
+  className?: string
+  placeholder?: string
+}
+
+
+export type IInput = ControlledInput
+
+const Input: React.FC<IInput> = ({
+  className = '',
+  placeholder = '',
+  value = '',
+  onChange,
+}) => {
+
+  const presentationStyles = `
+    ${styles.Input}
+    ${className}
+  `
+
+  return <>
+    <input
+      type="text"
+      className={presentationStyles}
+      value={value}
+      onChange={e => onChange(e.target.value)}
+      placeholder={placeholder}
+    />
+  </>
+}
+
+export { Input }
