00134f8fdb1471620178f6bae4f0170e3d481597
eli-d <64763513+eli-d@users.noreply.github.com>
2023-12-05 14:39:29 +1030

    update usage of mustEnv, isHash

--- web/src/util/tokens.ts
+++ web/src/util/tokens.ts
@@ -1,5 +1,4 @@
-import {Hash} from "viem"
-import {mustEnv} from "./env"
+import {Hash, isHex} from "viem"
 
 export interface Token {
   symbol: string
@@ -12,7 +11,16 @@ export interface Token {
   isFluidToken: boolean
 }
 
-const tokenListString = mustEnv('NEXT_PUBLIC_TOKEN_LIST')
+// TODO move all env stuff (including ammAddress) into own file
+const tokenListString = process.env.NEXT_PUBLIC_TOKEN_LIST
+if (!tokenListString)
+  throw new Error(`Failed to fetch required env NEXT_PUBLIC_TOKEN_LIST!`)
+
+const permit2AddressString = process.env.NEXT_PUBLIC_PERMIT2_ADDRESS
+if (!permit2AddressString || !isHex(permit2AddressString))
+  throw new Error(`PERMIT2 address from env was not a valid address! Was ${permit2AddressString}`)
+
+const permit2Address =  permit2AddressString
 
 const TokenList: Array<Token> = JSON.parse(tokenListString)
 const TokenMap: {[address: Hash]: Token} = TokenList.reduce((prev, current) => ({
@@ -22,14 +30,17 @@ const TokenMap: {[address: Hash]: Token} = TokenList.reduce((prev, current) => (
 
 const addressToSymbol = (address: Hash) => TokenMap[address].symbol
 
-const FluidTokenAddress = TokenList.find(({symbol}) => symbol === 'fUSDC')?.address
-
-if (!FluidTokenAddress)
-  throw new Error("Fluid Token not found in token list! (NEXT_PUBLIC_TOKEN_LIST)")
+const FluidTokenAddress = (() => {
+  const FluidTokenAddress = TokenList.find(({symbol}) => symbol === 'fUSDC')?.address
+  if (!FluidTokenAddress)
+    throw new Error("Fluid Token not found in token list! (NEXT_PUBLIC_TOKEN_LIST)")
+  return FluidTokenAddress
+})()
 
 export {
   TokenList,
   TokenMap,
   FluidTokenAddress,
+  permit2Address,
   addressToSymbol
 }
