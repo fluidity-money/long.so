5e751106ef91102cf7081457ae270e3ab95f683a
eli-d <64763513+eli-d@users.noreply.github.com>
2024-01-16 13:22:58 +1030

    update `useSwap` to use quoter

--- web/src/util/hooks/useSwap.ts
+++ web/src/util/hooks/useSwap.ts
@@ -1,12 +1,11 @@
-import {prepareWriteContract, signTypedData, writeContract} from 'wagmi/actions'
+import {prepareWriteContract, writeContract} from 'wagmi/actions'
 import {useContext, useEffect, useMemo, useState} from 'react'
 import SeawaterABI from '../abi/SeawaterAMM'
 import {ActiveTokenContext} from '../context/ActiveTokenContext'
 import {usePrepareContractWrite} from 'wagmi'
 import {FluidTokenAddress} from '../tokens'
-import {bigAbs} from '../math'
 import {useDebounce} from './useDebounce'
-import {hexToBigInt} from 'viem'
+import {Hash, hexToBigInt, maxUint256} from 'viem'
 import {usePermit2} from '../usePermit2'
 
 // the strongly typed return value of the Seawater contract function `T`
@@ -34,19 +33,15 @@ type SeawaterRequestArgs<T extends string> =
 // the possible function/argument combinations for swap simulations
 type PrepareContractState = 
 {
-    functionName: 'swapIn',
-    args: SeawaterRequestArgs<'swapIn'>
+    functionName: 'quote',
+    args: SeawaterRequestArgs<'quote'>
 } | 
 {
-    functionName: 'swapOut',
-    args: SeawaterRequestArgs<'swapOut'>
-} | 
-{
-    functionName: 'swap2ExactIn',
-    args: SeawaterRequestArgs<'swap2ExactIn'>
+    functionName: 'quote2',
+    args: SeawaterRequestArgs<'quote2'>
 }
 
-type permit2Function = `${'swapIn' | 'swapOut' | 'swap2ExactIn'}Permit2`
+type permit2Function = `${'swap' | 'swap2ExactIn'}Permit2`
 
 interface UseSwapProps {
     amountIn: bigint | string
@@ -58,10 +53,10 @@ interface UseSwapProps {
  */
 type UseSwap = ({amountIn, minOut}: UseSwapProps) => {
     /**
-     * @description - swap a token for another token by executing a permit2 contract call. The simulated result uses the non-permit2 version, as simulating a permit2 call requires user input (signature approval), and the regular call will have the same result. 
-     * Uses `token0` and `token1` from `ActiveTokenContext`, and `amountIn` and `minOut` from `UseSwapProps`. If one of the tokens is the fluid token, `swapInPermit2` or `swapOutPermit2` are called, otherwise `swap2ExactInPermit2` is called.
-     * @example amount 100 fUSDC -> token0 calls swapOutPermit2 - swap n token0 for 100 fusdc, result is [n, 100]
-     * @example amount 100 token0 -> fUSDC calls swapInPermit2  - swap 100 token0 for n fUSDC, result is [100, n]
+     * @description - swap a token for another token by executing a permit2 contract call. The simulated result uses `quote` and `quote2`, as simulating a permit2 call requires user input (signature approval), and the regular call requires ERC20 Approval.
+     * Uses `token0` and `token1` from `ActiveTokenContext`, and `amountIn` and `minOut` from `UseSwapProps`. If one of the tokens is the fluid token, `swapPermit2` is called, otherwise `swap2ExactInPermit2` is called.
+     * @example amount 100 fUSDC -> token0 calls swapPermit2 with zeroForOne = false - swap n token0 for 100 fusdc, result is [n, 100]
+     * @example amount 100 token0 -> fUSDC calls swapPermit2 with zeroForOne = true - swap 100 token0 for n fUSDC, result is [100, n]
      * @example amount 100 token0 -> token1 calls swap2ExactInPermit2 - swap 100 token0 for n token1, result is [100, n]
      */
     swap:  () => Promise<void>
@@ -69,7 +64,7 @@ type UseSwap = ({amountIn, minOut}: UseSwapProps) => {
     /**
      * @description - result of the simulated contract call with the current swap function and arguments. `result` is [bigint, bigint] if the function returns normally, otherwise undefined.
      **/
-    result: SeawaterResult<'swapIn' | 'swapOut' | 'swap2ExactIn'> | undefined
+    result: SeawaterResult<'swap' | 'swap2ExactIn'> | undefined
     
     /**
      * @description - the simulated function's error, as returned by Wagmi
@@ -87,24 +82,24 @@ const useSwap: UseSwap = ({amountIn, minOut}) => {
     const debouncedState = useDebounce(prepareContractState, 500)
 
     // we cannot reactively simulate permit2 without spamming the user for a signature
-    // instead, simulate the regular event (which has the same result), then create new args for permit2 when actually submitting to contract
+    // instead, use the quote function, then create new args for permit2 when actually submitting to contract
     // on update, set function name and arguments to trigger `usePrepareContractWrite`
     useEffect(() => {
         (async () => {
             try {
                 if (token0 === FluidTokenAddress) {
                     setPrepareContractState({
-                        functionName: 'swapOut',
-                        args: [token0, BigInt(amountIn), BigInt(minOut)]
+                        functionName: 'quote',
+                        args: [token0, false, BigInt(amountIn), maxUint256]
                     })
                 } else if (token1 === FluidTokenAddress) {
                     setPrepareContractState({
-                        functionName: 'swapIn',
-                        args: [token0, BigInt(amountIn), BigInt(minOut)]
+                        functionName: 'quote',
+                        args: [token0, true, BigInt(amountIn), maxUint256]
                     })
                 } else {
                     setPrepareContractState({
-                        functionName: 'swap2ExactIn',
+                        functionName: 'quote2',
                         args: [token0, token1, BigInt(amountIn), BigInt(minOut)]
                     })
                 }
@@ -124,14 +119,19 @@ const useSwap: UseSwap = ({amountIn, minOut}) => {
         args: debouncedState?.args,
     })
 
-    const result = useMemo(() =>
-        config.result?.map(n => bigAbs(n)) as [bigint, bigint] // assert type due to map's type erasure
-    , [config])
+    const result = useMemo(() => {
+        // parse the standard revert message to find the quote amount as a decimal number
+        const [, quoteAmountString] = error?.message.match(/reverted with the following reason:\n(.+)\n/) || []
+        return [
+            BigInt(amountIn ?? 0), 
+            BigInt(quoteAmountString ?? 0)
+        ] as const
+    }, [error])
 
     // initiate a swap as described in the hook's interface
     const swap = async() => {
         // simulation failed, so return early
-        if (error) {
+        if (error && result.every(r => r === BigInt(0))) {
             console.log('Error!',error)
             return
         }
@@ -144,16 +144,21 @@ const useSwap: UseSwap = ({amountIn, minOut}) => {
         const nonce = hexToBigInt(nonceHex)
         const maxAmount = amountIn
 
+        // determine which swap function to call
+        const swapFunction = debouncedState?.functionName === 'quote'
+            ? 'swap' // quote
+            : 'swap2ExactIn' // quote2
+
         // use the permit2 variant of the function
-        const functionName = debouncedState?.functionName + 'Permit2' as permit2Function
+        const functionName = swapFunction + 'Permit2' as permit2Function
 
         // append permit2-specific arguments
         const args = [
             ...debouncedState.args, 
             nonce, 
             encodedDeadline, 
-            // only swapIn and swapOut need maxAmount 
-            ...(debouncedState?.functionName === 'swap2ExactIn' ? [] : [maxAmount]),
+            // only swap needs maxAmount 
+            ...(swapFunction === 'swap2ExactIn' ? [] : [maxAmount]),
             sig,
         ]
 
