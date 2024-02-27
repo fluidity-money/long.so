4df073dac867a7b3223f3897dc5ee8dbaa631284
user <alex@iometelabs.io>
2024-02-27 15:39:46 +0800

    Add another test/make testing more obvious to outsiders

--- pkg/tests.sh
+++ pkg/tests.sh
@@ -1,3 +1,7 @@
 #!/bin/sh -e
 
-export RPC_URL=http://127.0.0.1:8080
+export RPC_URL=http://127.0.0.1:8547
+
+cargo test --features update_positions,testing
+
+node --test --loader tsx ethers-tests/test.ts
