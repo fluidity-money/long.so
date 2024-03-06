71abd22d1b6e9ccf1793d60df603623cdff21216
Aidan Kinzett <aidan@labrys.io>
2024-03-06 12:28:31 +1000

    add tailwind and shadcn/ui

--- web/src/components/Menu/Menu.tsx
+++ web/src/components/Menu/Menu.tsx
@@ -1,6 +1,6 @@
 import { LayoutGroup, motion } from 'framer-motion'
 import styles from './Menu.module.scss'
-import { Box, Text } from '..'
+import { Box } from '../index'
 
 interface IItem {
   children: React.ReactNode
@@ -26,34 +26,36 @@ const Item: React.FC<IItem> = (props) => {
     ${styles[background]}
   `
 
-  return <motion.div
-    className={classes}
-    whileHover={{
-      scale: !selected ? 1.05 : 1,
-      transition: {
-        duration: 0.4,
-        ease: 'easeInOut',
-      }
-    }}
-    whileTap={{
-      scale: !selected ? 0.95 : 1,
-      y: 1,
-      transition: {
-        duration: 0.2,
-        ease: 'easeInOut',
-      }
-    }}
-    onClick={onClick}
-  >
-    {
-      selected && <Box
-        layoutId={groupId}
-        background={background}
-        className={styles.virtualBox}
-      />
-    }
-    {children}
-  </motion.div>
+  return (
+    <motion.div
+      className={classes}
+      whileHover={{
+        scale: !selected ? 1.05 : 1,
+        transition: {
+          duration: 0.4,
+          ease: 'easeInOut',
+        },
+      }}
+      whileTap={{
+        scale: !selected ? 0.95 : 1,
+        y: 1,
+        transition: {
+          duration: 0.2,
+          ease: 'easeInOut',
+        },
+      }}
+      onClick={onClick}
+    >
+      {selected && (
+        <Box
+          layoutId={groupId}
+          background={background}
+          className={styles.virtualBox}
+        />
+      )}
+      {children}
+    </motion.div>
+  )
 }
 
 const Menu: React.FC<IMenu> = (props) => {
@@ -64,17 +66,30 @@ const Menu: React.FC<IMenu> = (props) => {
     ${styles[background]}
     ${styles[style]}
   `
-  const frameColor = (background === 'light' && style === 'primary') || (background === 'dark' && style === 'secondary') ? 'dark' : 'light'
+  const frameColor =
+    (background === 'light' && style === 'primary') ||
+    (background === 'dark' && style === 'secondary')
+      ? 'dark'
+      : 'light'
 
-  return <div className={classes}>
-    <LayoutGroup id={id}>
-      {children.map((item, i) => {
-        return <Item {...item.props} groupId={id} key={`${id}-${i}`} background={frameColor} />
-      })}
-    </LayoutGroup>
-  </div>
+  return (
+    <div className={classes}>
+      <LayoutGroup id={id}>
+        {children.map((item, i) => {
+          return (
+            <Item
+              {...item.props}
+              groupId={id}
+              key={`${id}-${i}`}
+              background={frameColor}
+            />
+          )
+        })}
+      </LayoutGroup>
+    </div>
+  )
 }
 
 export default Object.assign(Menu, {
-  Item
+  Item,
 })
