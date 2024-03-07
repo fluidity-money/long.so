ed1208c2b1de8adf62472f9d129c2aa602b245aa
user <alex@iometelabs.io>
2024-03-07 13:34:23 +0800

    Run more Stylus-hosted tests in tests.sh

--- pkg/tests.sh
+++ pkg/tests.sh
@@ -2,6 +2,10 @@
 
 export RPC_URL=http://127.0.0.1:8547
 
+cargo test --features admin,testing
 cargo test --features update_positions,testing
+cargo test --features swaps,testing
+cargo test --features swap_permit2,testing
+cargo test --features quotes,testing
 
 node --test --loader tsx ethers-tests/test.ts
