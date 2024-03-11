33e0c3be2109bd5a39450fbfdfca5b84c7b8dec2
Aidan Kinzett <aidan@labrys.io>
2024-03-11 22:06:52 +1000

    feat: make network selection into a radix dropdown

--- web/src/app/NetworkSelection.tsx
+++ web/src/app/NetworkSelection.tsx
@@ -1,41 +1,67 @@
-import { useState } from "react";
 import { Badge } from "@/components/ui/badge";
 import SPNTest from "@/assets/icons/spn-test.svg";
 import ArrowDown from "@/assets/icons/arrow-down.svg";
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+import { clsx } from "clsx";
 
 export const NetworkSelection = () => {
-  const [clicked, setClicked] = useState(false);
-
   return (
-    <Badge
-      variant="invert"
-      className={`group absolute right-44 hidden w-28 cursor-pointer px-0.5 transition-[width] hover:w-[120px] md:inline-flex ${
-        clicked ? "w-[120px] rounded-2xl" : ""
-      }`}
-      onClick={() => setClicked(!clicked)}
-    >
-      <div className={"flex-col"}>
-        <div className="flex flex-row items-center">
-          <div className="mr-2">
-            <SPNTest height={30} width={30} />
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger className="group data-[state=open]:bg-amber-400">
+        <Badge
+          variant="invert"
+          className={clsx(
+            "absolute",
+            "right-44",
+            "hidden",
+            "w-28",
+            "px-0.5",
+            "transition-[width]",
+            "group-hover:w-[120px]",
+            "group-data-[state=open]:w-[120px]",
+            "md:inline-flex",
+            "group-data-[state=open]:border-b-0",
+            "group-data-[state=open]:rounded-b-none",
+            "rounded-2xl",
+          )}
+        >
+          <div className={"flex-col"}>
+            <div className="flex flex-row items-center">
+              <div className="mr-2">
+                <SPNTest height={30} width={30} />
+              </div>
+              <div className="text-nowrap">SPN-Test</div>
+              <div
+                className={clsx(
+                  "ml-2",
+                  "transition-[width]",
+                  "group-hover:inline-flex",
+                  "group-hover:w-2",
+                  "group-data-[state=open]:inline-flex",
+                  "group-data-[state=open]:w-2",
+                  "hidden",
+                  "w-0",
+                )}
+              >
+                <ArrowDown width={10} height={6} />
+              </div>
+            </div>
           </div>
-          <div className="text-nowrap">SPN-Test</div>
-          <div
-            className={`ml-2 transition-[width] group-hover:inline-flex group-hover:w-2 ${
-              clicked ? "inline-flex w-2" : "hidden w-0"
-            }`}
-          >
-            <ArrowDown width={10} height={6} />
-          </div>
-        </div>
-        {clicked && (
-          <div className="flex flex-col gap-1 p-2">
-            <div>Arbitrum</div>
-            <div>Ethereum</div>
-            <div>Solana</div>
-          </div>
-        )}
-      </div>
-    </Badge>
+        </Badge>
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content className="relative -left-[62px] top-[34px] flex w-[120px] flex-col gap-1 rounded-2xl rounded-t-none border border-t-0 border-black bg-white p-2 ">
+          <DropdownMenu.Item className="cursor-pointer p-1 text-xs">
+            Ethereum
+          </DropdownMenu.Item>
+          <DropdownMenu.Item className="p-q cursor-pointer text-xs">
+            Arbitrum
+          </DropdownMenu.Item>
+          <DropdownMenu.Item className="cursor-pointer p-1 text-xs">
+            Solana
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
   );
 };
