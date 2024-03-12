22379b0d115d179db41d4716787086ac8228acc9
Aidan Kinzett <aidan@labrys.io>
2024-03-12 12:25:01 +1000

    feat: improve table in swap pro

--- web/src/app/_DataTable/columns.tsx
+++ web/src/app/_DataTable/columns.tsx
@@ -2,6 +2,9 @@
 
 import { ColumnDef } from "@tanstack/react-table";
 import { formatDistanceToNow } from "date-fns";
+import { Badge } from "@/components/ui/badge";
+import { Token } from "@/components";
+import { usdFormat } from "@/lib/usdFormat";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -10,16 +13,40 @@ export type Transaction = {
   value: number;
   rewards: number;
   time: Date;
+  amountFrom: number;
+  amountTo: number;
 };
 
 export const columns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "value",
     header: "Value",
+    cell: ({ row }) => usdFormat(row.original.value),
+  },
+  {
+    id: "amount",
+    header: "ƒUSDC/ETH",
+    cell: ({ row }) => {
+      return (
+        <div className="flex flex-row items-center gap-2">
+          <Token size="small" />
+          {row.original.amountFrom} {"->"} <Token size="small" />
+          {row.original.amountTo}
+        </div>
+      );
+    },
   },
   {
     accessorKey: "rewards",
     header: "Rewards",
+    cell: ({ row }) => {
+      return (
+        <Badge className="pl-0.5">
+          <Token size="small" />
+          <div className="ml-2">{row.original.rewards}</div>
+        </Badge>
+      );
+    },
   },
   {
     accessorKey: "time",
