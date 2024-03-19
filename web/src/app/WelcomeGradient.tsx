c5ea4941d7f001fc247af4b07e0adb51caf78451
Aidan Kinzett <aidan@labrys.io>
2024-03-19 11:17:57 +1000

    fix: fix width of hover trigger for welcome modal

--- web/src/app/WelcomeGradient.tsx
+++ web/src/app/WelcomeGradient.tsx
@@ -6,11 +6,13 @@ export const WelcomeGradient = () => {
   if (!welcome) return null;
 
   return (
-    <div
-      className="absolute top-[calc(50%-8rem)] z-[60] h-32 w-full cursor-pointer bg-gradient-to-b from-transparent to-white"
-      onMouseEnter={() => setHovering(true)}
-      onMouseLeave={() => setHovering(false)}
-      onClick={() => setWelcome(false)}
-    />
+    <div className="absolute top-[calc(50%-8rem)] z-[60] flex h-32 w-full flex-row justify-center bg-gradient-to-b from-transparent to-white">
+      <div
+        className="h-32 cursor-pointer sm:w-[400px] lg:w-[450px]"
+        onMouseEnter={() => setHovering(true)}
+        onMouseLeave={() => setHovering(false)}
+        onClick={() => setWelcome(false)}
+      />
+    </div>
   );
 };
