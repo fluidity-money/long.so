f65635b6fb675074b526a2bbb85acb25c7747f3e
Aidan Kinzett <aidan@labrys.io>
2024-03-20 15:53:41 +1000

    feat: replace yield over time chart with apache echarts

--- web/src/app/stake/YieldOverTimeGraph.tsx
+++ web/src/app/stake/YieldOverTimeGraph.tsx
@@ -1,248 +1,23 @@
 "use client";
 
 import { Menu } from "@/components";
-import { Bar, ComposedChart, ResponsiveContainer, Tooltip } from "recharts";
-import { subDays } from "date-fns";
-import { CustomTooltip } from "@/components/CustomTooltip";
 import { useState } from "react";
+import ReactECharts from "echarts-for-react";
+import * as echarts from "echarts/core";
 
-const data = [
-  {
-    name: "Page A",
-    date: new Date(),
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    date: subDays(new Date(), 1),
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    date: subDays(new Date(), 2),
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    date: subDays(new Date(), 3),
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    date: subDays(new Date(), 4),
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    date: subDays(new Date(), 5),
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    date: subDays(new Date(), 6),
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-  {
-    name: "Page H",
-    date: subDays(new Date(), 7),
-    uv: 3490,
-    pv: 4300,
-    amt: 2400,
-  },
-  {
-    name: "Page A",
-    date: subDays(new Date(), 8),
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    date: subDays(new Date(), 9),
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    date: subDays(new Date(), 10),
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    date: subDays(new Date(), 11),
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    date: subDays(new Date(), 12),
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    date: subDays(new Date(), 13),
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    date: subDays(new Date(), 14),
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-  {
-    name: "Page H",
-    date: subDays(new Date(), 15),
-    uv: 3490,
-    pv: 4300,
-    amt: 2400,
-  },
-  {
-    name: "Page A",
-    date: subDays(new Date(), 16),
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    date: subDays(new Date(), 17),
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    date: subDays(new Date(), 18),
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    date: subDays(new Date(), 19),
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    date: subDays(new Date(), 20),
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    date: subDays(new Date(), 21),
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    date: subDays(new Date(), 22),
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-  {
-    name: "Page H",
-    date: subDays(new Date(), 23),
-    uv: 3490,
-    pv: 4300,
-    amt: 2400,
-  },
-];
-
-/**
- * Top custom bar component to include borders, border radius, and linear gradients.
- *
- * The top two corners are rounded.
- * @param props
- * @constructor
- */
-const CustomBarTop = (props: any) => {
-  const { fill, x, y, width, height } = props;
-
-  // Calculate the radius for the top corners
-  const radius = 5; // Adjust this value as needed
-
-  // Draw the path with rounded top corners
-  const path = `M ${x},${y + radius}
-                a ${radius},${radius} 0 0 1 ${radius},-${radius}
-                h ${width - 2 * radius}
-                a ${radius},${radius} 0 0 1 ${radius},${radius}
-                v ${height - radius}
-                h -${width}
-                Z`;
-  return (
-    <g>
-      <path
-        d={path}
-        stroke="black"
-        strokeWidth={2}
-        fill={fill ?? `url(#gradientFill)`}
-      />
-    </g>
-  );
-};
-
-/**
- * Bottom custom bar component.
- *
- * The bottom two corners are rounded.
- * @param props
- * @constructor
- */
-const CustomBarBottom = (props: any) => {
-  const { fill, x, y, width, height } = props;
-
-  // Calculate the radius for the bottom corners
-  const radius = 5; // Adjust this value as needed
-
-  // Draw the path with rounded bottom corners
-  const path = `M ${x},${y}
-                v ${height - radius}
-                a ${radius},${radius} 0 0 0 ${radius},${radius}
-                h ${width - 2 * radius}
-                a ${radius},${radius} 0 0 0 ${radius},-${radius}
-                v -${height - radius}
-                Z`;
-
-  return (
-    <g>
-      <path
-        d={path}
-        stroke="black"
-        strokeWidth={2}
-        fill={fill ?? `url(#gradientFill)`}
-      />
-    </g>
-  );
-};
+const colorGradient = new echarts.graphic.LinearGradient(
+  0,
+  0,
+  0,
+  1, // Gradient direction from top(0,0) to bottom(0,1)
+  [
+    { offset: 0, color: "rgba(243, 184, 216, 0.4)" },
+    { offset: 0.25, color: "rgba(183, 147, 233, 0.4)" },
+    { offset: 0.5, color: "rgba(159, 212, 243, 0.4)" },
+    { offset: 0.75, color: "rgba(255, 210, 196, 0.4)" },
+    { offset: 1, color: "rgba(251, 243, 243, 0.4)" },
+  ],
+);
 
 export const YieldOverTimeGraph = () => {
   const [activeGraphDuration, setActiveGraphDuration] = useState<
@@ -295,58 +70,108 @@ export const YieldOverTimeGraph = () => {
 
       <div className="text-3xl">$12,500.42</div>
 
-      <div className="mt-16 flex flex-col gap-2">
-        <ResponsiveContainer height={150} width="100%">
-          <ComposedChart data={data} dataKey={"date"}>
-            <defs>
-              <linearGradient id="gradientFill" x1="0" y1="0" x2="0" y2="1">
-                <stop
-                  offset="0%"
-                  stopColor="rgba(243, 184, 216, 0.4)"
-                  stopOpacity={1}
-                />
-                <stop
-                  offset="25%"
-                  stopColor="rgba(183, 147, 233, 0.4)"
-                  stopOpacity={1}
-                />
-                <stop
-                  offset="50%"
-                  stopColor="rgba(159, 212, 243, 0.4)"
-                  stopOpacity={1}
-                />
-                <stop
-                  offset="75%"
-                  stopColor="rgba(255, 210, 196, 0.4)"
-                  stopOpacity={1}
-                />
-                <stop
-                  offset="100%"
-                  stopColor="rgba(251, 243, 243, 0.4)"
-                  stopOpacity={1}
-                />
-              </linearGradient>
-            </defs>
+      <div className="mt-8 flex flex-col gap-2">
+        <ReactECharts
+          className="h-[200px]"
+          opts={{
+            height: 200,
+          }}
+          style={{
+            height: 200,
+          }}
+          option={{
+            grid: {
+              left: "0", // or a small value like '10px'
+              right: "0", // or a small value
+              top: "0", // or a small value
+              bottom: "0", // or a small value
+            },
+            xAxis: {
+              type: "category",
+              data: [
+                "Mon",
+                "Tue",
+                "Wed",
+                "Thu",
+                "Fri",
+                "Sat",
+                "Sun",
+                "Mon",
+                "Tue",
+                "Wed",
+                "Thu",
+                "Fri",
+                "Sat",
+                "Sun",
+                "Mon",
+                "Tue",
+                "Wed",
+                "Thu",
+                "Fri",
+                "Sat",
+                "Sun",
+              ],
+              show: false,
+              axisPointer: {
+                label: {
+                  show: false,
+                },
+              },
+            },
+            yAxis: {
+              type: "value",
+              show: false,
+              axisPointer: {
+                label: {
+                  show: false,
+                },
+              },
+            },
+            series: [
+              {
+                name: "Series 2",
+                type: "bar",
+                stack: "total", // Same 'stack' value as Series 1 to stack them together
+                data: [
+                  120, 132, 101, 134, 90, 120, 132, 101, 134, 90, 120, 132, 101,
+                  134, 90, 120, 132, 101, 134, 90, 109,
+                ],
+                itemStyle: {
+                  // Custom styling for Series 2
+                  color: "black",
+                  barBorderRadius: [0, 0, 5, 5],
+                },
+                barWidth: "90%", // Adjust bar width (can be in pixels e.g., '20px')
+                barGap: "5%", // Adjust the gap between bars in different series
+              },
+              {
+                name: "series 1",
+                stack: "total",
+                data: [
+                  120, 200, 150, 80, 70, 110, 130, 120, 200, 150, 80, 70, 110,
+                  130, 120, 200, 150, 80, 70, 110, 130,
+                ],
+                type: "bar",
 
-            <Tooltip
-              content={<CustomTooltip />}
-              cursor={{
-                stroke: "black",
-                strokeDasharray: "3 3",
-              }}
-            />
+                itemStyle: {
+                  color: colorGradient,
 
-            {/* Bar for the black bottom section */}
-            <Bar
-              dataKey="uv"
-              stackId="stack"
-              shape={<CustomBarBottom fill="#000" />}
-            />
-
-            {/* Bar with the gradient on top */}
-            <Bar dataKey="pv" stackId="stack" shape={<CustomBarTop />} />
-          </ComposedChart>
-        </ResponsiveContainer>
+                  barBorderRadius: [5, 5, 0, 0], // Specify radius for all corners
+                  // Border configuration
+                  borderColor: "#000", // Border color
+                  borderWidth: 2, // Border width
+                  borderType: "solid", // Border type
+                },
+              },
+            ],
+            tooltip: {
+              trigger: "axis", // Trigger tooltip on axis movement
+              axisPointer: {
+                type: "cross", // Display crosshair style pointers
+              },
+            },
+          }}
+        />
 
         <div className="text-xs text-gray-2">Showing October 2023</div>
       </div>
