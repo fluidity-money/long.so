0530ec5f37803c96e6a168c82db5ab29eea33cd2
Aidan Kinzett <aidan@labrys.io>
2024-03-21 10:49:59 +1000

    feat: add desktop layout for create pool page

--- web/src/app/stake/pool/create/page.tsx
+++ web/src/app/stake/pool/create/page.tsx
@@ -10,29 +10,29 @@ export default function CreatePoolPage() {
         <CampaignBanner />
       </div>
 
-      <div className="mt-[12px] flex flex-col gap-[5px]">
+      <div className="mt-[12px] flex flex-col gap-[5px] md:gap-[7px]">
         <motion.div
           layoutId={"modal"}
-          className="flex h-[102px] w-[318px] flex-col gap-4 rounded-lg bg-black p-2 pt-0 text-white"
+          className="flex h-[102px] w-[318px] flex-col gap-4 rounded-lg bg-black p-2 pt-0 text-white md:h-[150px] md:w-[392px]"
         ></motion.div>
 
-        <div className="flex h-[102px] w-[318px] flex-col gap-4 rounded-lg bg-black p-2 pt-0 text-white"></div>
+        <div className="flex h-[102px] w-[318px] flex-col gap-4 rounded-lg bg-black p-2 pt-0 text-white md:h-[150px] md:w-[392px]"></div>
       </div>
 
-      <div className="mt-[12px] flex w-[318px] flex-row items-center justify-between">
-        <div className="text-3xs">Fee Tier</div>
+      <div className="mt-[12px] flex w-[318px] flex-row items-center justify-between md:w-[392px]">
+        <div className="text-3xs md:text-2xs">Fee Tier</div>
         <div className="h-[26px] w-[82px] rounded-md bg-black"></div>
       </div>
 
-      <div className="shine mt-[12px] h-[60px] w-[318px] rounded-lg"></div>
+      <div className="shine mt-[12px] h-[60px] w-[318px] rounded-lg md:h-[69px] md:w-[392px]"></div>
 
-      <div className="mt-[20px] h-[212px] w-[318px] rounded-lg bg-black"></div>
+      <div className="mt-[20px] h-[212px] w-[318px] rounded-lg bg-black md:h-[248px] md:w-[392px]"></div>
 
-      <div className="mt-[21px] flex w-[318px] flex-row justify-end">
+      <div className="mt-[21px] flex w-[318px] flex-row justify-end md:w-[392px]">
         <div className="text-2xs underline">Hide breakdown</div>
       </div>
 
-      <div className="mt-[15px] h-[200px] w-[318px] rounded-lg bg-black"></div>
+      <div className="mt-[15px] h-[200px] w-[318px] rounded-lg bg-black md:w-[392px]"></div>
 
       <div className="mt-[20px]"></div>
     </div>
