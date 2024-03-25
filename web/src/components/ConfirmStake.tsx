1bd24bfbe482519d31d7c173789de13a0b33da4c
Aidan Kinzett <aidan@labrys.io>
2024-03-25 12:13:30 +1000

    feat: add single token confirmation

--- web/src/components/ConfirmStake.tsx
+++ web/src/components/ConfirmStake.tsx
@@ -4,6 +4,8 @@ import { Button } from "@/components/ui/button";
 import Ethereum from "@/assets/icons/ethereum.svg";
 import { useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
+import { useStakeStore } from "@/stores/useStakeStore";
+import TokenIridescent from "@/assets/icons/token-iridescent.svg";
 
 interface ConfirmStakeProps {
   mode: "new" | "existing";
@@ -11,12 +13,16 @@ interface ConfirmStakeProps {
 
 export const ConfirmStake = ({ mode }: ConfirmStakeProps) => {
   const router = useRouter();
+
+  const multiSingleToken = useStakeStore((s) => s.multiSingleToken);
+
   return (
     <div className="z-10 flex flex-col items-center">
       <div
         className={cn("w-[315px] rounded-lg bg-black text-white md:w-[393px]", {
           "md:h-[685px]": mode === "existing",
-          "md:h-[673px] ": mode === "new",
+          "md:h-[673px]": mode === "new",
+          "md:h-[770px]": multiSingleToken === "single",
         })}
       >
         <div className="flex flex-row items-center justify-between p-[9px]">
@@ -35,7 +41,11 @@ export const ConfirmStake = ({ mode }: ConfirmStakeProps) => {
           </Button>
         </div>
 
-        <div className="mt-[26px] flex flex-col items-center md:mt-[30px]">
+        <div
+          className={cn("mt-[26px] flex flex-col items-center md:mt-[30px]", {
+            hidden: multiSingleToken === "single",
+          })}
+        >
           <div className="text-3xs md:text-2xs">
             {mode === "new"
               ? "Total Deposited Amount in"
@@ -54,7 +64,75 @@ export const ConfirmStake = ({ mode }: ConfirmStakeProps) => {
           </div>
         </div>
 
-        <div className="mt-[15px] pl-[21px]">
+        <div
+          className={cn("mt-[26px] flex flex-col items-center md:mt-[30px]", {
+            hidden: multiSingleToken === "multi",
+          })}
+        >
+          <div className="text-3xs md:text-2xs">
+            {mode === "new"
+              ? "Total Deposited Amount in"
+              : "Approximate Total Deposit Amount in"}{" "}
+            <span className="hidden md:inline-flex">
+              {" "}
+              <span className="font-medium underline">ƒUSDC</span>
+            </span>
+          </div>
+          <div className="mt-[4px] flex flex-row items-center gap-[6px] text-2xl font-medium md:text-3xl">
+            <TokenIridescent />
+            <div>700</div>
+          </div>
+          <div className="mt-[4px] text-3xs font-medium text-gray-2 md:text-2xs">
+            (= $700)
+          </div>
+          <div className="mt-[19px] w-[212px] text-center text-3xs font-medium text-gray-2 md:mt-[17px] md:w-[250px] md:text-2xs">
+            Your <span className="iridescent-text">700 ƒUSDC</span> will be
+            converted into the following two tokens to set your position in this
+            pool <div className="inline-block rotate-90">{"->"}</div>
+          </div>
+        </div>
+
+        <div
+          className={cn("mt-[20px] flex flex-col items-center", {
+            hidden: multiSingleToken === "multi",
+          })}
+        >
+          <div className="flex h-[156px] w-[272px] flex-col rounded-md border md:h-[199px] md:w-[349px]">
+            <div className="flex flex-1 flex-row justify-between border-b pl-[18px] pr-[26px] pt-[16px]">
+              <div className="flex w-full flex-col gap-1">
+                <div className="text-3xs md:text-2xs">ETH</div>
+                <div className="flex  flex-row items-center justify-between">
+                  <div className="flex flex-row items-center gap-1 text-sm md:gap-2 md:text-2xl">
+                    <Ethereum className={"invert"} />
+                    <div>0.1</div>
+                  </div>
+                  <div className="text-3xs text-gray-2 md:text-xs">50%</div>
+                </div>
+                <div className="text-3xs text-gray-2">= $350.00</div>
+              </div>
+            </div>
+
+            <div className="flex flex-1 flex-row justify-between pl-[18px] pr-[26px] pt-[16px]">
+              <div className="flex w-full flex-col gap-1">
+                <div className="text-3xs md:text-2xs">ƒUSDC</div>
+                <div className="flex  flex-row items-center justify-between">
+                  <div className="flex flex-row items-center gap-1 text-sm md:gap-2 md:text-2xl">
+                    <TokenIridescent />
+                    <div>350.00</div>
+                  </div>
+                  <div className="text-3xs text-gray-2 md:text-xs">50%</div>
+                </div>
+                <div className="text-3xs text-gray-2">= $350.00</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className={cn("mt-[15px] pl-[21px]", {
+            hidden: multiSingleToken === "single",
+          })}
+        >
           <div className="text-3xs font-medium md:text-2xs">ETH</div>
           <div className="mt-1 flex flex-row items-center gap-1 text-2xl">
             <Ethereum className={"invert"} /> 100.1234
@@ -64,7 +142,11 @@ export const ConfirmStake = ({ mode }: ConfirmStakeProps) => {
           </div>
         </div>
 
-        <div className="mt-[23px] pl-[21px]">
+        <div
+          className={cn("mt-[23px] pl-[21px]", {
+            hidden: multiSingleToken === "single",
+          })}
+        >
           <div className="text-3xs font-medium md:text-2xs">ƒUSDC</div>
           <div className="mt-1 flex flex-row items-center gap-1 text-2xl">
             <Ethereum className={"invert"} /> 100,230,989.00
@@ -81,7 +163,7 @@ export const ConfirmStake = ({ mode }: ConfirmStakeProps) => {
         <div
           className={cn("px-[21px]", {
             "mt-[29px] md:mt-[37px]": mode === "new",
-            "md:mt-[16px]": mode === "existing",
+            "mt-[19px] md:mt-[16px]": mode === "existing",
           })}
         >
           <div className="text-3xs font-medium md:text-2xs md:font-normal">
