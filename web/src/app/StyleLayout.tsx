eae1c4833fe7e6ac083b967c26339b76c3c0c43e
Aidan Kinzett <aidan@labrys.io>
2024-03-26 10:09:59 +1000

    feat: add label to button and title

--- web/src/app/StyleLayout.tsx
+++ web/src/app/StyleLayout.tsx
@@ -8,6 +8,7 @@ import { Button } from "@/components/ui/button";
 import { Menu, Text } from "@/components";
 import { Inter } from "next/font/google";
 import { useSwapPro } from "@/stores/useSwapPro";
+import Head from "next/head";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -21,62 +22,67 @@ export const StyleLayout = ({ children }: { children: React.ReactNode }) => {
   const { swapPro } = useSwapPro();
 
   return (
-    <body className={`${inter.className} flex flex-col bg-white`}>
-      <div className="iridescent-blur absolute left-1/2 top-[180px] size-full max-h-[305px] max-w-[557px] -translate-x-1/2" />
+    <>
+      <Head>
+        <title>Superposition AMM</title>
+      </Head>
+      <body className={`${inter.className} flex flex-col bg-white`}>
+        <div className="iridescent-blur absolute left-1/2 top-[180px] size-full max-h-[305px] max-w-[557px] -translate-x-1/2" />
 
-      <header className="p-8">
-        <div className="flex w-full flex-col gap-8">
-          <div className="flex flex-row items-start justify-between">
-            <div className="flex flex-row items-center gap-4">
-              <Superposition height={34} width={34} />
-              <MobileNetworkSelection />
+        <header className="p-8">
+          <div className="flex w-full flex-col gap-8">
+            <div className="flex flex-row items-start justify-between">
+              <div className="flex flex-row items-center gap-4">
+                <Superposition height={34} width={34} />
+                <MobileNetworkSelection />
+              </div>
+
+              <div className="flex flex-row items-start gap-4">
+                <NetworkSelection />
+                <Button size="sm" color="light" className="h-[26px] text-sm">
+                  Connect Wallet
+                </Button>
+              </div>
             </div>
-
-            <div className="flex flex-row items-start gap-4">
-              <NetworkSelection />
-              <Button size="sm" color="light" className="h-[26px] text-sm">
-                Connect Wallet
-              </Button>
+            <div className="flex w-full flex-col items-start md:items-center">
+              <Menu id="nav">
+                <Menu.Item
+                  onClick={() => {
+                    router.push("/");
+                  }}
+                  selected={pathname === "/" || pathname.startsWith("/swap")}
+                  proToggle
+                >
+                  <Text className="text-nowrap">
+                    Swap{" "}
+                    {swapPro && (
+                      <Text className="hidden md:inline-flex">{" Pro"}</Text>
+                    )}
+                  </Text>
+                </Menu.Item>
+                <Menu.Item
+                  className={"w-[73px]"}
+                  onClick={() => {
+                    router.push("/stake");
+                  }}
+                  selected={pathname.startsWith("/stake")}
+                >
+                  <Text>Stake</Text>
+                </Menu.Item>
+              </Menu>
             </div>
           </div>
-          <div className="flex w-full flex-col items-start md:items-center">
-            <Menu id="nav">
-              <Menu.Item
-                onClick={() => {
-                  router.push("/");
-                }}
-                selected={pathname === "/" || pathname.startsWith("/swap")}
-                proToggle
-              >
-                <Text className="text-nowrap">
-                  Swap{" "}
-                  {swapPro && (
-                    <Text className="hidden md:inline-flex">{" Pro"}</Text>
-                  )}
-                </Text>
-              </Menu.Item>
-              <Menu.Item
-                className={"w-[73px]"}
-                onClick={() => {
-                  router.push("/stake");
-                }}
-                selected={pathname.startsWith("/stake")}
-              >
-                <Text>Stake</Text>
-              </Menu.Item>
-            </Menu>
+        </header>
+
+        {children}
+
+        <footer className="w-full p-8">
+          <div className="flex flex-row justify-between">
+            <div>1002130192</div>
+            <div>Version 0.0.1</div>
           </div>
-        </div>
-      </header>
-
-      {children}
-
-      <footer className="w-full p-8">
-        <div className="flex flex-row justify-between">
-          <div>1002130192</div>
-          <div>Version 0.0.1</div>
-        </div>
-      </footer>
-    </body>
+        </footer>
+      </body>
+    </>
   );
 };
