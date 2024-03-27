9b3a9ba6df7cfc773bc667ade93e6059d609b308
Aidan Kinzett <aidan@labrys.io>
2024-03-27 10:56:05 +1000

    feat: add my transaction history table

--- web/src/app/ConnectWalletButton.tsx
+++ web/src/app/ConnectWalletButton.tsx
@@ -10,8 +10,372 @@ import { useDetectClickOutside } from "react-detect-click-outside";
 import CopyToClipboard from "react-copy-to-clipboard";
 import { Check } from "lucide-react";
 import { Menu } from "@/components";
+import { format, subDays, subHours, subMinutes, subWeeks } from "date-fns";
+import ReactECharts from "echarts-for-react";
+import { nanoid } from "nanoid";
+import Token from "@/assets/icons/token.svg";
+import Ethereum from "@/assets/icons/ethereum.svg";
+import { TransactionHistoryTable } from "@/app/_TransactionHistoryTable/TransactionHistoryTable";
+import {
+  columns,
+  TransactionHistory,
+} from "@/app/_TransactionHistoryTable/columns";
 
 const address = "0x0000000000000000000000000000000000000000";
+
+const data = [
+  {
+    name: "Page A",
+    date: new Date(),
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    date: subDays(new Date(), 1),
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    date: subDays(new Date(), 2),
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    date: subDays(new Date(), 3),
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    date: subDays(new Date(), 4),
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    date: subDays(new Date(), 5),
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    date: subDays(new Date(), 6),
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: "Page H",
+    date: subDays(new Date(), 7),
+    uv: 3490,
+    pv: 4300,
+    amt: 2400,
+  },
+  {
+    name: "Page A",
+    date: subDays(new Date(), 8),
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    date: subDays(new Date(), 9),
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    date: subDays(new Date(), 10),
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    date: subDays(new Date(), 11),
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    date: subDays(new Date(), 12),
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    date: subDays(new Date(), 13),
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    date: subDays(new Date(), 14),
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: "Page H",
+    date: subDays(new Date(), 15),
+    uv: 3490,
+    pv: 4300,
+    amt: 2400,
+  },
+  {
+    name: "Page A",
+    date: subDays(new Date(), 16),
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    date: subDays(new Date(), 17),
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    date: subDays(new Date(), 18),
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    date: subDays(new Date(), 19),
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    date: subDays(new Date(), 20),
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    date: subDays(new Date(), 21),
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    date: subDays(new Date(), 22),
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: "Page H",
+    date: subDays(new Date(), 23),
+    uv: 3490,
+    pv: 4300,
+    amt: 2400,
+  },
+];
+
+const transactionHistory: TransactionHistory[] = [
+  {
+    id: nanoid(),
+    tokens: [
+      {
+        name: "fUSDC",
+        icon: <Token />,
+      },
+      {
+        name: "ETH",
+        icon: <Ethereum className={"invert"} />,
+      },
+    ],
+    date: subMinutes(new Date(), 1),
+    rewards: 2.01,
+  },
+  {
+    id: nanoid(),
+    tokens: [
+      {
+        name: "ETH",
+        icon: <Ethereum className={"invert"} />,
+      },
+      {
+        name: "fUSDC",
+        icon: <Token />,
+      },
+    ],
+    date: subHours(new Date(), 1),
+    rewards: 12.33,
+  },
+  {
+    id: nanoid(),
+    tokens: [
+      {
+        name: "fUSDC",
+        icon: <Token />,
+      },
+      {
+        name: "ETH",
+        icon: <Ethereum className={"invert"} />,
+      },
+    ],
+    date: subDays(new Date(), 1),
+    rewards: 12.33,
+  },
+  {
+    id: nanoid(),
+    tokens: [
+      {
+        name: "ETH",
+        icon: <Ethereum className={"invert"} />,
+      },
+      {
+        name: "fUSDC",
+        icon: <Token />,
+      },
+    ],
+    date: subWeeks(new Date(), 1),
+    rewards: 12.33,
+  },
+  {
+    id: nanoid(),
+    tokens: [
+      {
+        name: "fUSDC",
+        icon: <Token />,
+      },
+      {
+        name: "ETH",
+        icon: <Ethereum className={"invert"} />,
+      },
+    ],
+    date: subWeeks(new Date(), 1),
+    rewards: 12.33,
+  },
+  {
+    id: nanoid(),
+    tokens: [
+      {
+        name: "fUSDC",
+        icon: <Token />,
+      },
+      {
+        name: "ETH",
+        icon: <Ethereum className={"invert"} />,
+      },
+    ],
+    date: subWeeks(new Date(), 1),
+    rewards: 12.33,
+  },
+  {
+    id: nanoid(),
+    tokens: [
+      {
+        name: "fUSDC",
+        icon: <Token />,
+      },
+      {
+        name: "ETH",
+        icon: <Ethereum className={"invert"} />,
+      },
+    ],
+    date: subWeeks(new Date(), 1),
+    rewards: 12.33,
+  },
+  {
+    id: nanoid(),
+    tokens: [
+      {
+        name: "fUSDC",
+        icon: <Token />,
+      },
+      {
+        name: "ETH",
+        icon: <Ethereum className={"invert"} />,
+      },
+    ],
+    date: subWeeks(new Date(), 1),
+    rewards: 12.33,
+  },
+  {
+    id: nanoid(),
+    tokens: [
+      {
+        name: "fUSDC",
+        icon: <Token />,
+      },
+      {
+        name: "ETH",
+        icon: <Ethereum className={"invert"} />,
+      },
+    ],
+    date: subWeeks(new Date(), 1),
+    rewards: 12.33,
+  },
+  {
+    id: nanoid(),
+    tokens: [
+      {
+        name: "fUSDC",
+        icon: <Token />,
+      },
+      {
+        name: "ETH",
+        icon: <Ethereum className={"invert"} />,
+      },
+    ],
+    date: subWeeks(new Date(), 1),
+    rewards: 12.33,
+  },
+  {
+    id: nanoid(),
+    tokens: [
+      {
+        name: "fUSDC",
+        icon: <Token />,
+      },
+      {
+        name: "ETH",
+        icon: <Ethereum className={"invert"} />,
+      },
+    ],
+    date: subWeeks(new Date(), 1),
+    rewards: 12.33,
+  },
+  {
+    id: nanoid(),
+    tokens: [
+      {
+        name: "fUSDC",
+        icon: <Token />,
+      },
+      {
+        name: "ETH",
+        icon: <Ethereum className={"invert"} />,
+      },
+    ],
+    date: subWeeks(new Date(), 1),
+    rewards: 12.33,
+  },
+];
 
 export const ConnectWalletButton = () => {
   const [isConnected, setIsConnected] = useState(false);
@@ -42,6 +406,10 @@ export const ConnectWalletButton = () => {
     };
   }, [copied]);
 
+  const [activeGraphDuration, setActiveGraphDuration] = useState<
+    "7D" | "1M" | "6M" | "1Y" | "ALL"
+  >("7D");
+
   if (isConnected) {
     return (
       <Sheet>
@@ -58,7 +426,7 @@ export const ConnectWalletButton = () => {
           />
         </div>
 
-        <SheetContent className="my-2 rounded-lg border-0 bg-black text-white">
+        <SheetContent className="my-2 max-h-screen overflow-y-scroll rounded-lg border-0 bg-black text-white">
           <div className="flex flex-row items-center justify-between">
             <div className="flex flex-row items-center gap-1">
               <Image
@@ -144,15 +512,133 @@ export const ConnectWalletButton = () => {
               <div className={"text-sm font-medium "}>
                 My Total Trade Rewards
               </div>
+
               <Badge
                 variant={"iridescent"}
                 className={"mt-[12px] text-3xl font-medium"}
               >
                 $1,337
               </Badge>
+
               <div className="mt-[28px] w-[223px] text-center text-[10px] font-normal text-neutral-400">
                 Earn more by making more transactions!
               </div>
+
+              <div className="fledx mt-[42px] flex w-full flex-row items-center justify-between">
+                <div className="text-[10px] font-medium">
+                  Trader Rewards Over Time
+                </div>
+                <Menu
+                  id="trader-rewards-over-time"
+                  background={"dark"}
+                  className={"gap-1"}
+                >
+                  <Menu.Item
+                    selected={activeGraphDuration === "7D"}
+                    onClick={() => setActiveGraphDuration("7D")}
+                  >
+                    <div className="px-1 text-[10px]">7D</div>
+                  </Menu.Item>
+                  <Menu.Item
+                    selected={activeGraphDuration === "1M"}
+                    onClick={() => setActiveGraphDuration("1M")}
+                  >
+                    <div className="px-1 text-[10px]">1M</div>
+                  </Menu.Item>
+                  <Menu.Item
+                    selected={activeGraphDuration === "6M"}
+                    onClick={() => setActiveGraphDuration("6M")}
+                  >
+                    <div className="px-1 text-[10px]">6M</div>
+                  </Menu.Item>
+                  <Menu.Item
+                    selected={activeGraphDuration === "1Y"}
+                    onClick={() => setActiveGraphDuration("1Y")}
+                  >
+                    <div className="px-1 text-[10px]">1Y</div>
+                  </Menu.Item>
+                  <Menu.Item
+                    selected={activeGraphDuration === "ALL"}
+                    onClick={() => setActiveGraphDuration("ALL")}
+                  >
+                    <div className="px-1 text-[10px]">All</div>
+                  </Menu.Item>
+                </Menu>
+              </div>
+
+              <ReactECharts
+                className="mt-[20px] h-[70px] w-full"
+                opts={{
+                  height: 70,
+                }}
+                style={{
+                  height: 70,
+                }}
+                option={{
+                  grid: {
+                    left: "0", // or a small value like '10px'
+                    right: "0", // or a small value
+                    top: "0", // or a small value
+                    bottom: "0", // or a small value
+                  },
+                  tooltip: {
+                    trigger: "axis", // Trigger tooltip on axis movement
+                    axisPointer: {
+                      type: "cross", // Display crosshair style pointers
+                    },
+                    borderWidth: 0,
+                    backgroundColor: "#EBEBEB",
+                    textStyle: {
+                      color: "#1E1E1E",
+                    },
+                    formatter:
+                      "<div class='flex flex-col items-center'>${c} <div class='text-gray-2 text-center w-full'>{b}</div></div>",
+                  },
+                  xAxis: {
+                    type: "category",
+                    data: data.map((d) => format(d.date, "P")),
+                    show: false,
+                    axisPointer: {
+                      label: {
+                        show: false,
+                      },
+                    },
+                  },
+                  yAxis: {
+                    type: "value",
+                    show: false,
+                    axisPointer: {
+                      label: {
+                        show: false,
+                      },
+                    },
+                  },
+                  series: [
+                    {
+                      type: "bar",
+                      data: data.map((d) => d.uv),
+                      itemStyle: {
+                        color: "#EBEBEB",
+                      },
+                      barWidth: "70%", // Adjust bar width (can be in pixels e.g., '20px')
+                      barGap: "30%", // Adjust the gap between bars in different series
+                    },
+                  ],
+                }}
+              />
+
+              <div className={"mt-[13px] w-full text-left text-[10px]"}>
+                22nd February 2024
+              </div>
+
+              <div className="mt-[30px] w-full text-left text-[10px]">
+                My Transaction History
+              </div>
+
+              <TransactionHistoryTable
+                columns={columns}
+                data={transactionHistory}
+              />
             </div>
           )}
         </SheetContent>
