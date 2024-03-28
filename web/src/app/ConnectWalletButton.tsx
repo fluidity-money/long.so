bc62fb3bf162020fbb23a7ed292b0bfdb54b5ba3
Aidan Kinzett <aidan@labrys.io>
2024-03-28 14:04:38 +1000

    feat: connect claim yield button to claim flow

--- web/src/app/ConnectWalletButton.tsx
+++ web/src/app/ConnectWalletButton.tsx
@@ -1,10 +1,9 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import { InventoryContent } from "@/components/InventoryContent";
 import { useConnectionStore } from "@/stores/useConnectionStore";
 import { useMediaQuery } from "@/hooks/useMediaQuery";
 import { usePathname, useRouter } from "next/navigation";
+import { InventorySheet } from "@/components/InventorySheet";
 
 const address = "0x0000000000000000000000000000000000000000";
 
@@ -17,26 +16,7 @@ export const ConnectWalletButton = () => {
   const pathname = usePathname();
 
   if (isConnected && !isLtSm) {
-    return (
-      <Sheet>
-        <div className="flex flex-row items-center justify-center gap-[10px] rounded">
-          <SheetTrigger asChild>
-            <div className="cursor-pointer text-nowrap rounded p-1 text-right text-xs font-semibold text-black transition-all hover:bg-black hover:text-base hover:text-white">
-              {address.slice(0, 5)} ... {address.slice(-3)}
-            </div>
-          </SheetTrigger>
-          <Image
-            src={require("@/assets/profile-picture.png")}
-            alt={"profile picture"}
-            className={"size-[28px] rounded"}
-          />
-        </div>
-
-        <SheetContent className="my-2 max-h-screen overflow-y-scroll rounded-lg border-0 bg-black text-white">
-          <InventoryContent />
-        </SheetContent>
-      </Sheet>
-    );
+    return <InventorySheet />;
   }
 
   if (isConnected && isLtSm && pathname === "/swap/inventory") {
