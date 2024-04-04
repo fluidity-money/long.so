497416b0fb9e1135c35a274ca7d2b82671ed604f
Aidan Kinzett <aidan@labrys.io>
2024-04-04 16:02:12 +1000

    fix: add as const

--- web/src/config/index.ts
+++ web/src/config/index.ts
@@ -10,25 +10,22 @@ export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
 if (!projectId) throw new Error("Project ID is not defined");
 
 const metadata = {
-  name: "Web3Modal",
+  name: "Superposition",
   description: "Web3Modal Example",
   url: "https://web3modal.com", // origin must match your domain & subdomain
   icons: ["https://avatars.githubusercontent.com/u/37784886"],
 };
-
-// const multicall3AddressString = "";
-// if (!multicall3AddressString || !isHex(multicall3AddressString))
-//   throw new Error(
-//     `Multicall3 address from env was not a valid address! Was ${multicall3AddressString}`,
-//   );
-// const multicall3Address = multicall3AddressString;
 
 const arbitrumStylusTestnet = {
   ...arbitrumSepolia,
   name: "Stylus Testnet",
   id: 23011913,
   // use a self-deployed multicall3 for Stylus Testnet as it doesn't support the standard address
-  // contracts: { multicall3: { address: multicall3Address } },
+  contracts: {
+    multicall3: {
+      address: "0x42aaE78422EF3e8E6d0D88e58E25CA7C7Ecb9D5a" as const,
+    },
+  },
   rpcUrls: {
     default: { http: ["https://stylus-testnet.arbitrum.io/rpc"] },
     public: { http: ["https://stylus-testnet.arbitrum.io/rpc"] },
