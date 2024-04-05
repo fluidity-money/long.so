98a655ae7b3db24f6a448da0f1dda37bbf37646c
Aidan Kinzett <aidan@labrys.io>
2024-04-05 10:25:12 +1000

    feat: use ens name in place of wallet address if available

--- web/src/components/InventorySheet.tsx
+++ web/src/components/InventorySheet.tsx
@@ -2,19 +2,29 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Image from "next/image";
 import { InventoryContent } from "@/components/InventoryContent/InventoryContent";
 import { useInventorySheet } from "@/stores/useInventorySheet";
-import { useAccount } from "wagmi";
+import { useAccount, useEnsName } from "wagmi";
 
 export const InventorySheet = () => {
   const { isOpen, setIsOpen } = useInventorySheet();
 
   const { address } = useAccount();
 
+  const { data: ensName } = useEnsName({
+    address,
+  });
+
   return (
     <Sheet open={isOpen} onOpenChange={setIsOpen}>
       <div className="flex flex-row items-center justify-center gap-[10px] rounded">
         <SheetTrigger asChild>
           <div className="cursor-pointer text-nowrap rounded p-1 text-right text-xs font-semibold text-black transition-all hover:bg-black hover:text-base hover:text-white">
-            {address?.slice(0, 5)} ... {address?.slice(-3)}
+            {ensName ? (
+              ensName
+            ) : (
+              <>
+                {address?.slice(0, 5)} ... {address?.slice(-3)}
+              </>
+            )}
           </div>
         </SheetTrigger>
         <Image
