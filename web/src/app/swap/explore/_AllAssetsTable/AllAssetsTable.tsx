5c4c97dee40d833fc32611954e655e9bfd04702a
Aidan Kinzett <aidan@labrys.io>
2024-04-05 14:45:09 +1000

    feat: add filter to table

--- web/src/app/swap/explore/_AllAssetsTable/AllAssetsTable.tsx
+++ web/src/app/swap/explore/_AllAssetsTable/AllAssetsTable.tsx
@@ -2,8 +2,10 @@
 
 import {
   ColumnDef,
+  FilterFn,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 
@@ -15,24 +17,108 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import Search from "@/assets/icons/Search.svg";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useState } from "react";
+import { rankItem } from "@tanstack/match-sorter-utils";
 
 interface AllPoolsTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
   data: TData[];
+  children?: React.ReactNode;
 }
+
+const fuzzyFilter: FilterFn<any> = (row, columnId, value, addMeta) => {
+  // Rank the item
+  const itemRank = rankItem(row.getValue(columnId), value);
+
+  // Store the itemRank info
+  addMeta({
+    itemRank,
+  });
+
+  // Return if the item should be filtered in/out
+  return itemRank.passed;
+};
+
+/**
+ * Hide the address column from the UI. This is included
+ * so that the filter can search by address, but the column
+ * is not shown in the UI.
+ */
+const columnVisibility = {
+  address: false,
+};
 
 export function AllAssetsTable<TData, TValue>({
   columns,
   data,
+  children,
 }: AllPoolsTableProps<TData, TValue>) {
+  const [globalFilter, setGlobalFilter] = useState("");
+
   const table = useReactTable({
     data,
     columns,
+    filterFns: {
+      fuzzy: fuzzyFilter,
+    },
+    state: {
+      globalFilter,
+      columnVisibility,
+    },
     getCoreRowModel: getCoreRowModel(),
+    globalFilterFn: fuzzyFilter,
+    getFilteredRowModel: getFilteredRowModel(),
   });
 
   return (
-    <div className="rounded-lg ">
+    <div>
+      <Label htmlFor={"filter"} className={"text-[10px] md:text-[12px]"}>
+        Filter
+      </Label>
+      <div className="flex flex-row items-center border-b border-white">
+        <Input
+          value={globalFilter}
+          onChange={(e) => setGlobalFilter(e.target.value)}
+          variant="no-ring"
+          className="h-8 border-0 bg-transparent text-[10px] md:text-[12px]"
+          placeholder={"e.g. Ether, ARB, 0x0bafe8babf38bf3ba83fb80a82..."}
+        />
+        <Search className="size-4" />
+      </div>
+      {children}
+      <div className={"mt-[26px] flex flex-row items-center justify-between"}>
+        <div className={"text-[10px] md:text-[12px]"}>All</div>
+        <div
+          className={
+            "flex flex-row items-center gap-1 text-[10px] md:text-[12px]"
+          }
+        >
+          <div className={"text-nowrap"}>Sort by</div>
+          <Select defaultValue={"campaigns"}>
+            <SelectTrigger
+              className={
+                "h-auto rounded-none border-0 border-b border-dashed bg-transparent p-0 text-[10px] md:text-[12px]"
+              }
+            >
+              <SelectValue />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value={"campaigns"}>campaigns</SelectItem>
+              <SelectItem value={"popularity"}>popularity</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
       <Table>
         <TableHeader className="[&_tr]:border-b-0">
           {table.getHeaderGroups().map((headerGroup) => (
