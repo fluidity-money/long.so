64ba866f574cda410a1323145d6d06a514b4cd92
Aidan Kinzett <aidan@labrys.io>
2024-04-08 11:54:26 +1000

    feat: add settings page to inventory

--- web/src/components/InventoryContent/InventoryHeader.tsx
+++ web/src/components/InventoryContent/InventoryHeader.tsx
@@ -2,8 +2,13 @@ import Image from "next/image";
 import Cog from "@/assets/icons/cog.svg";
 import { DisconnectButton } from "@/components/InventoryContent/DisconnectButton";
 import { Address } from "@/components/InventoryContent/Address";
+import { Badge } from "@/components/ui/badge";
+import { cn } from "@/lib/utils";
+import { useInventorySettings } from "@/components/InventoryContent/useInventorySettings";
 
 export const InventoryHeader = () => {
+  const { settings, setSettings } = useInventorySettings();
+  
   return (
     <div className="flex w-full flex-row items-center justify-between">
       <div className="flex flex-row items-center gap-1">
@@ -17,7 +22,24 @@ export const InventoryHeader = () => {
       </div>
 
       <div className="flex flex-row items-center gap-[20px]">
-        <Cog />
+        <Badge
+          variant="secondary"
+          className={cn(
+            "size-[25px] cursor-pointer items-center gap-1 px-1 transition-all",
+            {
+              "bg-transparent": !settings,
+              "w-[80px] ": settings,
+            },
+          )}
+          onClick={() => setSettings(!settings)}
+        >
+          <Cog
+            className={cn("size-[15px]", {
+              invert: settings,
+            })}
+          />
+          {settings && "Settings"}
+        </Badge>
         <DisconnectButton />
       </div>
     </div>
