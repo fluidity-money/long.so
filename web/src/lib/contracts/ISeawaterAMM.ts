a739677105437f844719e4e25ad0212f4fd3a797
Aidan Kinzett <aidan@labrys.io>
2024-04-17 17:21:49 +1000

    feat: show balance of selected token in stake screen

--- web/src/lib/contracts/ISeawaterAMM.ts
+++
@@ -1,9 +0,0 @@
-import { getContract } from "viem";
-import { output } from "@/lib/abi/ISeawaterAMM";
-import { publicClient } from "@/lib/publicClient";
-
-export const ISeawaterAMM = getContract({
-  address: "0x6b3a8A49FA004757B72AbF111CC5fE1e4869dF2e",
-  abi: output.abi,
-  client: publicClient,
-});
