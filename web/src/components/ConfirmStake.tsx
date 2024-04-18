18c6b92b5a87bf62e1cff3bad471cc723a0420dc
Aidan Kinzett <aidan@labrys.io>
2024-04-18 15:17:16 +1000

    feat: connect stake form to contracts

--- web/src/components/ConfirmStake.tsx
+++ web/src/components/ConfirmStake.tsx
@@ -7,6 +7,18 @@ import { cn } from "@/lib/utils";
 import { useStakeStore } from "@/stores/useStakeStore";
 import TokenIridescent from "@/assets/icons/token-iridescent.svg";
 import { motion } from "framer-motion";
+import {
+  useAccount,
+  useSimulateContract,
+  useWaitForTransactionReceipt,
+  useWriteContract,
+} from "wagmi";
+import { output } from "@/lib/abi/ISeawaterAMM";
+import { encodeTick } from "@/lib/math";
+import { useEffect } from "react";
+import { erc20Abi, Hash, hexToBigInt, maxUint256 } from "viem";
+import { ammAddress } from "@/lib/addresses";
+import LightweightERC20 from "@/lib/abi/LightweightERC20";
 
 interface ConfirmStakeProps {
   mode: "new" | "existing";
@@ -16,6 +28,102 @@ export const ConfirmStake = ({ mode }: ConfirmStakeProps) => {
   const router = useRouter();
 
   const multiSingleToken = useStakeStore((s) => s.multiSingleToken);
+  const token0 = useStakeStore((s) => s.token0);
+  const token0Amount = useStakeStore((s) => s.token0Amount);
+
+  const { writeContract, data, error } = useWriteContract();
+  console.log(error);
+  const createPosition = () => {
+    console.log("creating position");
+
+    writeContract({
+      address: ammAddress,
+      abi: output.abi,
+      functionName: "mintPosition",
+      args: [token0.address, encodeTick(50), encodeTick(100)],
+    });
+  };
+
+  const result = useWaitForTransactionReceipt({
+    hash: data,
+  });
+
+  const mintPositionId = result.data?.logs[0].topics[1];
+
+  useEffect(() => {
+    if (!mintPositionId) return;
+
+    approve();
+  }, [mintPositionId]);
+
+  const {
+    writeContract: writeContractUpdatePosition,
+    data: updatePositionData,
+    error: updatePositionError,
+  } = useWriteContract();
+
+  console.log(updatePositionData);
+  console.log(updatePositionError);
+
+  const { address } = useAccount();
+
+  console.log(address);
+  const { data: allowanceData, error: allowanceError } = useSimulateContract({
+    address: token0.address,
+    abi: LightweightERC20,
+    // @ts-ignore this needs to use useSimulateContract which breaks the types
+    functionName: "allowance",
+    // @ts-ignore
+    args: [address as Hash, ammAddress],
+  });
+
+  console.log("allowance", allowanceData?.result);
+
+  const updatePosition = (id: bigint) => {
+    console.log("updating position: ", id);
+
+    const delta = BigInt(token0Amount);
+
+    writeContractUpdatePosition({
+      address: ammAddress,
+      abi: output.abi,
+      functionName: "updatePosition",
+      args: [token0.address, id, delta],
+    });
+  };
+
+  const {
+    writeContract: writeContractApproval,
+    data: approvalData,
+    error: approvalError,
+  } = useWriteContract();
+
+  const approvalResult = useWaitForTransactionReceipt({
+    hash: approvalData,
+  });
+
+  console.log(approvalData);
+  console.log(approvalError);
+
+  const approve = () => {
+    if (!allowanceData?.result || allowanceData.result === BigInt(0)) {
+      console.log("approving");
+      writeContractApproval({
+        address: token0.address,
+        abi: erc20Abi,
+        functionName: "approve",
+        args: [ammAddress, maxUint256],
+      });
+    } else {
+      updatePosition(hexToBigInt(mintPositionId as Hash));
+    }
+  };
+
+  useEffect(() => {
+    if (!approvalResult.data || !mintPositionId) return;
+
+    updatePosition(hexToBigInt(mintPositionId));
+  }, [approvalResult.data, mintPositionId]);
 
   return (
     <div className="z-10 flex flex-col items-center">
@@ -101,11 +209,11 @@ export const ConfirmStake = ({ mode }: ConfirmStakeProps) => {
           <div className="flex h-[156px] w-[272px] flex-col rounded-md border md:h-[199px] md:w-[349px]">
             <div className="flex flex-1 flex-row justify-between border-b pl-[18px] pr-[26px] pt-[16px]">
               <div className="flex w-full flex-col gap-1">
-                <div className="text-3xs md:text-2xs">ETH</div>
+                <div className="text-3xs md:text-2xs">{token0.symbol}</div>
                 <div className="flex  flex-row items-center justify-between">
                   <div className="flex flex-row items-center gap-1 text-sm md:gap-2 md:text-2xl">
                     <Ethereum className={"invert"} />
-                    <div>0.1</div>
+                    <div>{token0Amount}</div>
                   </div>
                   <div className="text-3xs text-gray-2 md:text-xs">50%</div>
                 </div>
@@ -134,13 +242,13 @@ export const ConfirmStake = ({ mode }: ConfirmStakeProps) => {
             hidden: multiSingleToken === "single",
           })}
         >
-          <div className="text-3xs font-medium md:text-2xs">ETH</div>
+          <div className="text-3xs font-medium md:text-2xs">
+            {token0.symbol}
+          </div>
           <div className="mt-1 flex flex-row items-center gap-1 text-2xl">
-            <Ethereum className={"invert"} /> 100.1234
+            <Ethereum className={"invert"} /> {token0Amount}
           </div>
-          <div className="mt-0.5 text-2xs text-gray-2 md:text-xs">
-            = $350.00
-          </div>
+          <div className="mt-0.5 text-2xs text-gray-2 md:text-xs">= $?</div>
         </div>
 
         <div
@@ -236,7 +344,11 @@ export const ConfirmStake = ({ mode }: ConfirmStakeProps) => {
         </div>
 
         <div className=" flex flex-col items-center p-[15px]">
-          <Button variant={"secondary"} className="w-full max-w-[350px]">
+          <Button
+            variant={"secondary"}
+            className="w-full max-w-[350px]"
+            onClick={() => createPosition()}
+          >
             Confirm Stake
           </Button>
         </div>
---
+++ web/src/lib/addresses.ts
@@ -0,0 +1,1 @@
+export const ammAddress = "0x4c74ba6cE8469d74A075Bf72BaB3B97F40F9Db56";
