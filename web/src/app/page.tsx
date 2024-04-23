6eab0fdc2ef1281a107f66bff77ed17bee946df5
Aidan Kinzett <aidan@labrys.io>
2024-04-23 11:03:00 +1000

    test: test hydration error with datatable commented out

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,12 +1,13 @@
 import { Welcome } from "@/app/Welcome";
 
 import { SwapForm } from "@/app/SwapForm";
+import { SwapPro } from "@/components/SwapPro";
 
 export default function Swap() {
   return (
     <div className="relative flex w-full flex-col">
       <div className="flex max-w-full flex-col-reverse justify-center gap-8 lg:flex-row">
-        {/*<SwapPro />*/}
+        <SwapPro />
         <SwapForm />
       </div>
 
