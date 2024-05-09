bc28af1e47033ab8ae5495ec9bcec75dfbea2315
bayge <alex@fluidity.money>
2024-05-09 17:37:06 +0100

    Add coverage testing, naive beginnings of delta function

--- web/jest.config.ts
+++ web/jest.config.ts
@@ -13,6 +13,17 @@ const config: Config = {
     "/node_modules/"
   ],
 
+  collectCoverage: true,
+
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
 
