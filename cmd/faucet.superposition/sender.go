bb4f6861d392965a8ae9e274f1f5d4745e8c80ee
bayge <alex@fluidity.money>
2024-06-11 00:27:24 +0100

    Nil out the graph

--- cmd/faucet.superposition/sender.go
+++ cmd/faucet.superposition/sender.go
@@ -2,6 +2,7 @@ package main
 
 import "github.com/fluidity-money/long.so/cmd/faucet.superposition/graph"
 
-func RunSender() (c chan graph.FaucetReq<-) {
+func RunSender() (c chan<- graph.FaucetReq) {
 	//TODO
+	return nil
 }
