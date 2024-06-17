fa3c6a575cfafae8bc9aeb28c306097444dc1817
eli-d <64763513+eli-d@users.noreply.github.com>
2024-06-17 17:16:39 +0930

    use compact notation for currencies

--- web/src/lib/usdFormat.tsx
+++ web/src/lib/usdFormat.tsx
@@ -2,5 +2,7 @@ export const usdFormat = (value: number) => {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
+    notation: "compact",
+    minimumFractionDigits: 2,
   }).format(value);
 };
