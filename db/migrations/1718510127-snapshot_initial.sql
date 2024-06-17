65863aa74ba9f998a6258442303f950d7efe1781
bayge <alex@fluidity.money>
2024-06-17 23:32:56 +0100

    Restore the table for latest snapshots

--- db/migrations/1718510127-snapshot_initial.sql
+++ db/migrations/1718510127-snapshot_initial.sql
@@ -1,5 +1,23 @@
 -- migrate:up
 
+-- The latest for everything, so we can quickly use it to get the liquidity in pools.
+CREATE TABLE snapshot_positions_latest_1 (
+	id SERIAL PRIMARY KEY,
+	created_by TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+
+	-- Taken from a left join.
+	pos_id HUGEINT NOT NULL,
+	owner ADDRESS NOT NULL,
+	pool ADDRESS NOT NULL,
+	lower BIGINT NOT NULL,
+	upper BIGINT NOT NULL,
+
+	-- Inserted from the lookup.
+	amount0 HUGEINT NOT NULL,
+	amount1 HUGEINT NOT NULL
+);
+
+-- The rolling log for historical data reasons.
 CREATE TABLE snapshot_positions_log_1 (
 	id SERIAL PRIMARY KEY,
 	created_by TIMESTAMP WITHOUT TIME ZONE NOT NULL,
@@ -21,7 +39,32 @@ RETURNS VOID LANGUAGE PLPGSQL
 AS $$
 DECLARE affected_rows INT;
 BEGIN
+	DELETE FROM snapshot_positions_latest_1;
 	FOR i IN 1..array_length(ids, 1) LOOP
+		INSERT INTO snapshot_positions_latest_1 (
+			updated_by,
+			pos_id,
+			owner,
+			pool,
+			lower,
+			upper,
+			amount0,
+			amount1
+		)
+		SELECT
+			CURRENT_TIMESTAMP,
+			sw.pos_id,
+			sw.owner,
+			sw.pool,
+			sw.lower,
+			sw.upper,
+			amount0s[i],
+			amount1s[i]
+		FROM
+			events_seawater_mintPosition sw
+		WHERE
+			sw.pos_id = ids[i];
+
 		INSERT INTO snapshot_positions_log_1 (
 			created_by,
 			pos_id,
