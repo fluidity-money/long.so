3c412236a50024ec94490f73ef5fa4f731a03d7d
eli-d <64763513+eli-d@users.noreply.github.com>
2024-06-20 18:48:52 +0930

    scale price over time by ticks

--- cmd/graphql.ethereum/graph/model/price.go
+++ cmd/graphql.ethereum/graph/model/price.go
@@ -1,22 +1,17 @@
 package model
 
 import (
-	"fmt"
-	"math"
-
+	"github.com/fluidity-money/long.so/lib/math"
 	"github.com/fluidity-money/long.so/lib/types"
 )
 
 type PriceResult struct {
-	FinalTick types.Number  `json:"final_tick"`
+	FinalTick types.Number `json:"final_tick"`
 }
 
 // Price to obtain the price from the final tick as a formatted float string
 func (p PriceResult) Price(fusdcDecimals, poolDecimals int) string {
-	// p(i)) = 1.0001^i * 10**(fUSDC decimals - pool decimals)
-	tick, _ := p.FinalTick.Float64()
-	base := float64(10001) / 10000
-	decimals := math.Pow10(fusdcDecimals - poolDecimals)
-	price := math.Pow(base, tick) * decimals
-	return fmt.Sprintf("%0.4f", price)
+	sqrtPrice := math.GetSqrtRatioAtTick(p.FinalTick.Big())
+	price := math.GetPriceAtSqrtRatio(sqrtPrice)
+	return price.FloatString(5)
 }
