224f5fa936e23676930a4cffbc486da7105ac195
eli-d <64763513+eli-d@users.noreply.github.com>
2024-06-20 14:09:41 +0930

    add new tokens

--- web/src/config/tokens.ts
+++ web/src/config/tokens.ts
@@ -19,9 +19,33 @@ export const DefaultToken: Token = {
   decimals: 18,
 };
 
+export const USDC: Token = {
+  name: "USD Coin",
+  address: "0x6437fdc89ced41941b97a9f1f8992d88718c81c5",
+  symbol: "USDC",
+  decimals: 6,
+}
+
+export const WETH: Token = {
+  name: "Wrapped Ethereum",
+  address: "0xde104342b32bca03ec995f999181f7cf1ffc04d7",
+  symbol: "WETH",
+  decimals: 18,
+}
+
+export const WSPN: Token = {
+  name: "Wrapped Superposition",
+  address: "0x22b9fa698b68bba071b513959794e9a47d19214c",
+  symbol: "WSPN",
+  decimals: 18,
+}
+
 const allTokens: Token[] = [
   fUSDC,
   DefaultToken,
+  USDC,
+  WSPN,
+  WETH,
 ];
 
 export const getTokenFromAddress = (address_: string): Token | undefined =>
