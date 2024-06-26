4d6859b19bbb055032408afa2e2e74c620dc5b8a
bayge <alex@fluidity.money>
2024-06-26 15:31:37 +0930

    Rip out the server behaviour to somewhere else

--- cmd/faucet.superposition/graph/resolver.go
+++ cmd/faucet.superposition/graph/resolver.go
@@ -4,7 +4,6 @@ import (
 	"gorm.io/gorm"
 
 	ethCommon "github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/ethclient"
 
 	"github.com/fluidity-money/long.so/lib/config"
 	"github.com/fluidity-money/long.so/lib/features"
@@ -14,9 +13,7 @@ type (
 	Resolver struct {
 		DB                   *gorm.DB          // db used to look up any fields that are missing from a request.
 		F                    features.F        // features to have enabled when requested
-		GethSepolia, GethSpn *ethclient.Client // needed to do lookups with geth
 		C                    config.C          // config for connecting to the right endpoints
-		Queue                chan<- FaucetReq  // queue for faucet requests
 	}
 
 	// FaucetReq to an IP address given, assuming they passed the
--- cmd/faucet.superposition/main.go
+++ cmd/faucet.superposition/main.go
@@ -4,7 +4,6 @@ package main
 
 import (
 	"context"
-	"crypto/ecdsa"
 	_ "embed"
 	"net/http"
 	"os"
@@ -14,14 +13,9 @@ import (
 	"github.com/fluidity-money/long.so/lib/setup"
 
 	"github.com/fluidity-money/long.so/cmd/faucet.superposition/graph"
-	"github.com/fluidity-money/long.so/cmd/faucet.superposition/lib/faucet"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
-
-	ethCommon "github.com/ethereum/go-ethereum/common"
-	ethCrypto "github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/ethclient"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -77,68 +71,11 @@ func main() {
 	if err != nil {
 		setup.Exitf("database open: %v", err)
 	}
-	// Get the private key to use to make transactions to the faucet with later.
-	key_ := os.Getenv(EnvPrivateKey)
-	if key_ == "" {
-		setup.Exitf("%#v unset", EnvPrivateKey)
-	}
-	key, err := ethCrypto.HexToECDSA(key_)
-	if err != nil {
-		setup.Exitf("private key: %v", err)
-	}
-	var (
-		faucetAddrSepolia = ethCommon.HexToAddress(os.Getenv(EnvFaucetAddrSepolia))
-		faucetAddrSpn     = ethCommon.HexToAddress(os.Getenv(EnvFaucetAddrSpn))
-	)
-	senderPub, _ := key.Public().(*ecdsa.PublicKey) // Should be fine.
-	senderAddr := ethCrypto.PubkeyToAddress(*senderPub)
-	// Configure the RPCs.
-	sepoliaUrl := os.Getenv(EnvSepoliaUrl)
-	if sepoliaUrl == "" {
-		setup.Exitf("sepolia url empty. set %#v", EnvSepoliaUrl)
-	}
-	gethSepolia, err := ethclient.Dial(sepoliaUrl)
-	if err != nil {
-		setup.Exitf("geth open sepolia: %v", err)
-	}
-	defer gethSepolia.Close()
-	gethSpn, err := ethclient.Dial(config.GethUrl)
-	if err != nil {
-		setup.Exitf("geth open spn: %v", err)
-	}
-	defer gethSpn.Close()
-	defer gethSepolia.Close()
-	// Get the chain id for sending out requests to the faucet.
-	chainIdSepolia, err := gethSepolia.ChainID(context.Background())
-	if err != nil {
-		setup.Exitf("chain id seoplia: %v", err)
-	}
-	chainIdSpn, err := gethSpn.ChainID(context.Background())
-	if err != nil {
-		setup.Exitf("chain id spn: %v", err)
-	}
-	// Start the sender in another Go routine to send batch requests
-	// out of the SPN (gas) token.
-	queue := RunSender(
-		gethSepolia,
-		gethSpn,
-		chainIdSepolia,
-		chainIdSpn,
-		key,
-		senderAddr,
-		faucetAddrSepolia,
-		faucetAddrSpn,
-		faucet.SendFaucet,
-		faucet.WaitMined,
-	)
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{
 		Resolvers: &graph.Resolver{
 			DB:          db,
 			F:           features.Get(),
-			GethSepolia: gethSepolia,
-			GethSpn:     gethSpn,
 			C:           config,
-			Queue:       queue,
 		},
 	}))
 	// Add a custom transport so we can access the requesting IP address in a context.
