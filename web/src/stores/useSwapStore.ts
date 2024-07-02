c599dfe44652747aa99ded5b148b4ee10619e9cd
eli-d <64763513+eli-d@users.noreply.github.com>
2024-07-02 14:28:03 +0930

    check for empty string when flipping tokens

--- web/src/stores/useSwapStore.ts
+++ web/src/stores/useSwapStore.ts
@@ -39,8 +39,8 @@ export const useSwapStore = create<SwapStore>((set) => ({
     set(({ token0, token1, token1Amount, token0Amount, token0AmountRaw, token1AmountRaw }) => ({
       token0: token1,
       token1: token0,
-      token0Amount: token1Amount === "." ? "0" : token1Amount,
-      token1Amount: token0Amount === "." ? "0" : token0Amount,
+      token0Amount: token1Amount === "." || token1Amount === "" ? "0" : token1Amount,
+      token1Amount: token0Amount === "." || token0Amount === "" ? "0" : token0Amount,
       token0AmountRaw: token1AmountRaw,
       token1AmountRaw: token0AmountRaw,
     }))
@@ -91,5 +91,5 @@ export const useSwapStore = create<SwapStore>((set) => ({
   },
 
   gas: 0n,
-  setGas: gas => set({gas})
+  setGas: gas => set({ gas })
 }));
