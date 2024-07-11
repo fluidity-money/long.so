dad0d2486e5ccc29b8db48d1606bbcefe0b490de
ogous <oguzxan.ali@gmail.com>
2024-07-11 18:56:09 +0300

    fix: wait for third page to navigate

--- web/e2e/app.spec.ts
+++ web/e2e/app.spec.ts
@@ -3,12 +3,12 @@ import { test, expect } from "./fixtures";
 test("should navigate to the stake page", async ({ page }) => {
   await page.goto("/");
 
-  // await expect(page.getByTestId("third-page-anchor")).toContainText(
-  //   "Go to third page",
-  // );
-  // await page.getByTestId("third-page-anchor").click();
-  await page.click("text=Go to third page");
-  await page.waitForURL("/third", { timeout: 60000, waitUntil: "load" });
+  await page.waitForSelector("data-test=third-page-anchor");
+
+  await expect(page.getByTestId("third-page-anchor")).toContainText(
+    "Go to third page",
+  );
+  await page.getByTestId("third-page-anchor").click();
   await expect(page).toHaveURL("/third");
   await expect(page.locator("h3")).toContainText("Third page");
 });
