40c8a4aa28090edf575dd367f4cc82947f6cce49
bayge <alex@fluidity.money>
2024-07-12 19:48:44 +0930

    Start to support a display configuration graph

--- cmd/graphql.ethereum/graph/resolver.go
+++ cmd/graphql.ethereum/graph/resolver.go
@@ -7,11 +7,13 @@ import (
 
 	"github.com/fluidity-money/long.so/lib/config"
 	"github.com/fluidity-money/long.so/lib/features"
+	"github.com/fluidity-money/long.so/lib/types"
 )
 
 type Resolver struct {
-	DB   *gorm.DB          // db used to look up any fields that are missing from a request.
-	F    features.F        // features to have enabled when requested
-	Geth *ethclient.Client // needed to do lookups with geth
-	C    config.C          // config for connecting to the right endpoints
+	DB          *gorm.DB                      // db used to look up any fields that are missing from a request.
+	F           features.F                    // features to have enabled when requested
+	Geth        *ethclient.Client             // needed to do lookups with geth
+	C           config.C                      // config for connecting to the right endpoints
+	PoolsConfig map[types.Address]config.Pool // config for pools deployed only the backend knows.
 }
