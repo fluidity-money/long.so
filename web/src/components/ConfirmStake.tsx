f0f10a88cb76e44201d1ee9c0efd54ec21ed3655
eli-d <64763513+eli-d@users.noreply.github.com>
2024-07-16 13:43:47 +0930

    redirect to stake home after staking

--- web/src/components/ConfirmStake.tsx
+++ web/src/components/ConfirmStake.tsx
@@ -340,7 +340,7 @@ export const ConfirmStake = ({ mode, positionId }: ConfirmStakeProps) => {
         resetApproveToken0()
         resetApproveToken1()
         updatePositionResult.refetch()
-        router.push("/")
+        router.push("/stake")
       }}
     />;
   }
