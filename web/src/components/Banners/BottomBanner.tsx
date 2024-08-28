8b158042797c395aa4f7e191318b21a355895d27
ogous <oguzxan.ali@gmail.com>
2024-08-28 12:08:35 +0300

    feat: customize bottom banner

--- web/src/components/Banners/BottomBanner.tsx
+++ web/src/components/Banners/BottomBanner.tsx
@@ -3,6 +3,7 @@ import { graphql, useFragment } from "@/gql";
 import { useFeatureFlag } from "@/hooks/useFeatureFlag";
 import { useGraphqlUser } from "@/hooks/useGraphql";
 import { cn } from "@/lib/utils";
+import { useState } from "react";
 const NotesFragment = graphql(`
   fragment NotesFragment on Note {
     content
@@ -15,16 +16,28 @@ export default function BottomBanner() {
   const notes = useFragment(NotesFragment, data?.notes);
   const bottomNote = notes?.find((item) => item.placement.includes("bottom"));
   const showBanners = useFeatureFlag("ui show banners");
+  const [isBannerClosed, setIsBannerClosed] = useState(false);
 
-  if (!bottomNote || !showBanners) return null;
+  if (!bottomNote || !showBanners || isBannerClosed) return null;
 
+  const isLeftBanner = bottomNote.placement.includes("left");
   return (
     <div
-      dangerouslySetInnerHTML={{ __html: bottomNote?.content }}
       className={cn(
-        "fixed bottom-4",
-        bottomNote?.placement.includes("left") ? "left-4" : "right-4",
+        "fixed bottom-16 z-20  ",
+        isLeftBanner ? "left-4" : "right-4",
       )}
-    />
+    >
+      <div
+        dangerouslySetInnerHTML={{ __html: bottomNote.content }}
+        className="banner flex max-h-32 max-w-64 rounded-md bg-stone-900"
+      ></div>
+      <div
+        onClick={() => setIsBannerClosed(true)}
+        className="absolute right-2 top-2 flex cursor-pointer items-center justify-center rounded-md bg-white px-3 py-1 text-sm"
+      >
+        x
+      </div>
+    </div>
   );
 }
