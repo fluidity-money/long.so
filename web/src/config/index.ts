eef70d231cb2ece0d62a0d4056ee549db864b158
ogous <oguzxan.ali@gmail.com>
2024-09-11 18:49:58 +0300

    fix: seperate server env from config

--- web/src/config/index.ts
+++ web/src/config/index.ts
@@ -1,10 +1,8 @@
 import clientEnv from "./clientEnv";
-import serverEnv from "./serverEnv";
 import appConfig from "./app";
 import wagmiConfig from "./wagmi";
 import * as networks from "./networks";
 import * as tokens from "./networks";
 
 const config = { ...clientEnv, ...appConfig, wagmiConfig, networks, tokens };
-export { serverEnv };
 export default config;
