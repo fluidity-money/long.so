5d3f8c4d4ba5538234006db719b4d84d016003d3
ogous <oguzxan.ali@gmail.com>
2024-09-11 22:34:08 +0300

    feat: generate transports from default http of chains

--- web/src/config/wagmi.ts
+++ web/src/config/wagmi.ts
@@ -1,18 +1,22 @@
 "use client";
 
 import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
-import { http } from "viem";
+import { http, HttpTransport } from "viem";
 import * as chains from "./chains";
 import appConfig from "./app";
+
+const testnetTransports = chains.allTestnets.reduce(
+  (acc, chain) => {
+    acc[chain.id] = http(chain.rpcUrls.default.http[0]);
+    return acc;
+  },
+  {} as Record<number, HttpTransport>,
+);
 
 const wagmiConfig = defaultWagmiConfig({
   chains: chains.allTestnets,
   projectId: process.env.NEXT_PUBLIC_LONGTAIL_WALLETCONNECT_PROJECT_ID,
-  transports: {
-    [chains.superpositionTestnet.id]: http(
-      chains.superpositionTestnet.rpcUrls.default.http[0],
-    ),
-  },
+  transports: testnetTransports,
   metadata: appConfig.metadata,
   ssr: true,
 });
