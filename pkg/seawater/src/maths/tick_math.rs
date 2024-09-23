a8ae14a05a591e3738cf93c68b5821227d60f2dd
bayge <alex@fluidity.money>
2024-09-23 20:38:06 +0930

    Codearena 259, X96 sqrt ratio calculated is wrong
    We had the original implementation when codesize was a major issue. Now that things are
    better, we can more closely match Uniswap's implementation. With some testing, we found
    that it is now 1 to 1 with the correct behaviour.

--- pkg/seawater/src/maths/tick_math.rs
+++ pkg/seawater/src/maths/tick_math.rs
@@ -18,40 +18,86 @@ pub const MAX_SQRT_RATIO: U256 =
 
 /// Calculates the X96 encoded square root price for a given tick index.
 pub fn get_sqrt_ratio_at_tick(tick: i32) -> Result<U256, Error> {
-    let mut abs_tick = tick.abs();
+    let abs_tick = if tick < 0 {
+        U256::from(-tick)
+    } else {
+        U256::from(tick)
+    };
 
-    if abs_tick > MAX_TICK {
+    if abs_tick > U256::from(MAX_TICK) {
         return Err(Error::T);
     }
 
-    // calculate (1/1.0001)^(i/2)
-    // = (1/1.0001)^(floor(i/2)) * maybe (1/1.0001)^(0.5)
-
-    let mut result = if (abs_tick & 0x1) != 0 {
-        uint!(0xfffcb933bd6fad37aa2d162d1a594001_U256) // 1 * (1/1.0001)^0.5
+    let mut ratio = if abs_tick & (U256::from(0x1)) != U256::zero() {
+        uint!(0xfffcb933bd6fad37aa2d162d1a594001_U256)
     } else {
-        U256::one() << 128 // 1
+        uint!(0x100000000000000000000000000000000_U256)
     };
 
-    abs_tick >>= 1;
-
-    let mut ratio = uint!(0xfff97272373d413259a46990580e213a_U256); // 1/1.0001
-
-    while abs_tick != 0 {
-        if (abs_tick & 1) != 0 {
-            result = (result * ratio) >> 128;
-        }
-        ratio = (ratio * ratio) >> 128;
-        abs_tick >>= 1;
+    if !(abs_tick & (U256::from(0x2))).is_zero() {
+        ratio = (ratio * uint!(0xfff97272373d413259a46990580e213a_U256)) >> 128
+    }
+    if !(abs_tick & (U256::from(0x4))).is_zero() {
+        ratio = (ratio * uint!(0xfff2e50f5f656932ef12357cf3c7fdcc_U256)) >> 128
+    }
+    if !(abs_tick & (U256::from(0x8))).is_zero() {
+        ratio = (ratio * uint!(0xffe5caca7e10e4e61c3624eaa0941cd0_U256)) >> 128
+    }
+    if !(abs_tick & (U256::from(0x10))).is_zero() {
+        ratio = (ratio * uint!(0xffcb9843d60f6159c9db58835c926644_U256)) >> 128
+    }
+    if !(abs_tick & (U256::from(0x20))).is_zero() {
+        ratio = (ratio * uint!(0xff973b41fa98c081472e6896dfb254c0_U256)) >> 128
+    }
+    if !(abs_tick & (U256::from(0x40))).is_zero() {
+        ratio = (ratio * uint!(0xff2ea16466c96a3843ec78b326b52861_U256)) >> 128
+    }
+    if !(abs_tick & (U256::from(0x80))).is_zero() {
+        ratio = (ratio * uint!(0xfe5dee046a99a2a811c461f1969c3053_U256)) >> 128
+    }
+    if !(abs_tick & (U256::from(0x100))).is_zero() {
+        ratio = (ratio * uint!(0xfcbe86c7900a88aedcffc83b479aa3a4_U256)) >> 128
+    }
+    if !(abs_tick & (U256::from(0x200))).is_zero() {
+        ratio = (ratio * uint!(0xf987a7253ac413176f2b074cf7815e54_U256)) >> 128
+    }
+    if !(abs_tick & (U256::from(0x400))).is_zero() {
+        ratio = (ratio * uint!(0xf3392b0822b70005940c7a398e4b70f3_U256)) >> 128
+    }
+    if !(abs_tick & (U256::from(0x800))).is_zero() {
+        ratio = (ratio * uint!(0xe7159475a2c29b7443b29c7fa6e889d9_U256)) >> 128
+    }
+    if !(abs_tick & (U256::from(0x1000))).is_zero() {
+        ratio = (ratio * uint!(0xd097f3bdfd2022b8845ad8f792aa5825_U256)) >> 128
+    }
+    if !(abs_tick & (U256::from(0x2000))).is_zero() {
+        ratio = (ratio * uint!(0xa9f746462d870fdf8a65dc1f90e061e5_U256)) >> 128
+    }
+    if !(abs_tick & (U256::from(0x4000))).is_zero() {
+        ratio = (ratio * uint!(0x70d869a156d2a1b890bb3df62baf32f7_U256)) >> 128
+    }
+    if !(abs_tick & (U256::from(0x8000))).is_zero() {
+        ratio = (ratio * uint!(0x31be135f97d08fd981231505542fcfa6_U256)) >> 128
+    }
+    if !(abs_tick & (U256::from(0x10000))).is_zero() {
+        ratio = (ratio * uint!(0x9aa508b5b7a84e1c677de54f3e99bc9_U256)) >> 128
+    }
+    if !(abs_tick & (U256::from(0x20000))).is_zero() {
+        ratio = (ratio * uint!(0x5d6af8dedb81196699c329225ee604_U256)) >> 128
+    }
+    if !(abs_tick & (U256::from(0x40000))).is_zero() {
+        ratio = (ratio * uint!(0x2216e584f5fa1ea926041bedfe98_U256)) >> 128
+    }
+    if !(abs_tick & (U256::from(0x80000))).is_zero() {
+        ratio = (ratio * uint!(0x48a170391f7dc42444e8fa2_U256)) >> 128
     }
 
-    // invert back from 1/1.0001^i to 1.0001^i
     if tick > 0 {
-        result = U256::MAX / result;
+        ratio = U256::MAX / ratio;
     }
 
-    Ok((result >> 32)
-        + if (result % (U256::one() << 32) as U256).is_zero() {
+    Ok((ratio >> 32)
+        + if (ratio % (U256::one() << 32) as U256).is_zero() {
             U256::zero()
         } else {
             U256::one()
--- pkg/seawater/tests/reference_impls.rs
+++ pkg/seawater/tests/reference_impls.rs
@@ -69,39 +69,6 @@ fn test_get_tick_at_sqrt_ratio() {
     assert!(errs < 10);
 }
 
-#[test]
-fn test_get_sqrt_ratio_at_tick() {
-    const ONE_HUNDRETH_BPS: U256 = uint!(0x10c6f7a0b5ed8d36b4c7f3493858_U256);
-
-    let mut rng = rand::thread_rng();
-    let mut errs: i64 = 0;
-    for _ in 0..1000 {
-        let tick = rng.gen_range(tick_math::MIN_TICK..=tick_math::MAX_TICK);
-
-        let ratio = tick_math::get_sqrt_ratio_at_tick(tick);
-
-        #[cfg(feature = "testing-dbg")]
-        dbg!(
-            ("tick", current_test!(), tick),
-            ("ratio", current_test!(), &ratio)
-        );
-
-        let reference = reference::tick_math::get_sqrt_ratio_at_tick(tick);
-        if ratio.is_err() {
-            errs += 1;
-            assert!(ratio.is_err() && reference.is_err());
-        } else {
-            let ratio = ratio.unwrap();
-            let reference = reference.unwrap();
-            let abs_diff = max(ratio, reference) - min(ratio, reference);
-            // uniswap allows for an error of less than 1/100 bps
-            assert!(abs_diff < ONE_HUNDRETH_BPS);
-        }
-    }
-    // make sure that we're actually testing the function
-    assert!(errs < 10);
-}
-
 mod test_mul_div_more {
     use crate::reference;
 
