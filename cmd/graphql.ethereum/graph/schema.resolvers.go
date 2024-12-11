ba238e674fe3bc57f974f6da23090fefb9bca76b
eli-d <64763513+eli-d@users.noreply.github.com>
2024-12-11 15:24:35 +1030

    scale fusdc ValueUsd by the correct number of decimals

--- cmd/graphql.ethereum/graph/schema.resolvers.go
+++ cmd/graphql.ethereum/graph/schema.resolvers.go
@@ -123,7 +123,7 @@ func (r *amountResolver) ValueUsd(ctx context.Context, obj *model.Amount) (strin
 	// If the pool is the fUSDC address, then we can just skip the
 	// lookup here and report $1 (assuming we maintain the peg.)
 	if obj.Token == r.C.FusdcAddr {
-		return obj.ValueUnscaled.ScaleStr(5), nil
+		return obj.ValueUnscaled.ScaleStr(6), nil
 	}
 	// Use the final tick function to get the row.
 	var finalTick struct {
