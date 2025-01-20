93316211bb8c894bccf9a74f3f5b4c1074a40e3a
eli-d <64763513+eli-d@users.noreply.github.com>
2025-01-20 16:50:24 +1030

    use new function in resolver

--- cmd/graphql.ethereum/graph/schema.resolvers.go
+++ cmd/graphql.ethereum/graph/schema.resolvers.go
@@ -1274,7 +1274,7 @@ func (r *seawaterPoolResolver) Swaps(ctx context.Context, obj *seawater.Pool, fi
 	}
 	// DB.RAW doesn't support chaining
 	err = r.DB.Raw(
-		"SELECT * FROM seawater_swaps_pool_5(?, ?, ?, ?, ?, ?)",
+		"SELECT * FROM seawater_swaps_pool_6(?, ?, ?, ?, ?, ?)",
 		r.C.FusdcAddr,
 		r.C.FusdcDecimals,
 		obj.Token,
