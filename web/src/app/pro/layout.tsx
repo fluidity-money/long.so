9530b8e6110207087063595767801d28dde4917f
ogous <oguzxan.ali@gmail.com>
2025-03-27 14:36:21 +0200

    fit pro screen to device height

--- web/src/app/pro/layout.tsx
+++ web/src/app/pro/layout.tsx
@@ -10,9 +10,7 @@ const inter = Inter({
 });
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div
-      className={cn("flex min-h-screen flex-col bg-[#000]", inter.className)}
-    >
+    <div className={cn("flex h-[100vh] flex-col bg-[#000]", inter.className)}>
       <div className="iridescent-blur absolute left-1/2 top-[180px] size-full max-h-[305px] max-w-[557px] -translate-x-1/2" />
       <Header isDark={true} />
       <div className={"z-10 flex flex-1 flex-col"}>{children}</div>
--- web/src/app/pro/page.tsx
+++ web/src/app/pro/page.tsx
@@ -7,7 +7,7 @@ import TokenDetails from "@/components/Pro/TokenDetails";
 export default function ProMode() {
   return (
     <div className="flex flex-1 gap-4 px-4">
-      <div className="flex flex-[4] flex-col gap-4">
+      <div className="flex grow flex-col gap-4">
         <ProHeader />
         <div className="flex grow items-center justify-center rounded-lg bg-white/10 text-white">
           Middle Left/Candlestick Chart
@@ -16,10 +16,12 @@ export default function ProMode() {
           <DataScene />
         </div>
       </div>
-      <div className="flex flex-1 flex-col gap-4 border-l border-black px-2">
-        <Call2Action />
-        <DurationControl />
-        <TokenDetails />
+      <div className="relative w-[300px]">
+        <div className="absolute inset-0 flex flex-col gap-4 overflow-y-auto border-l border-black px-2">
+          <Call2Action />
+          <DurationControl />
+          <TokenDetails />
+        </div>
       </div>
     </div>
   );
