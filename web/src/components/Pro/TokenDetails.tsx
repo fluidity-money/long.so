c8054365d38af22c7247e423bbc5d88f5563e34e
ogous <oguzxan.ali@gmail.com>
2025-04-14 15:51:21 +0300

    add copy icon to pair details

--- web/src/components/Pro/TokenDetails.tsx
+++ web/src/components/Pro/TokenDetails.tsx
@@ -1,14 +1,15 @@
 import config from "@/config/app";
-
+import CopyIcon from "@/assets/icons/copy.svg";
 export default function TokenDetails({
   pair,
 }: {
   pair: (typeof config.pairs)[number];
 }) {
+  const names = pair.name.split("/");
   const data = {
     pair: pair.pair,
-    token0: pair.quoteToken0,
-    token1: pair.quoteToken1,
+    [names[0]]: pair.quoteToken0,
+    [names[1]]: pair.quoteToken1,
   };
   return (
     <div className="flex flex-col gap-2 px-2">
@@ -25,9 +26,12 @@ export default function TokenDetails({
           <span className="text-[10px] font-normal text-gray-200 capitalize">
             {key.replace("_", " ")}
           </span>
-          <span className="rounded-full bg-zinc-800 px-2 py-1 text-[10px] text-zinc-400">
-            {value.slice(0, 6)}...{value.slice(-6)}
-          </span>
+          <div className="flex items-center gap-1 rounded-full bg-zinc-800 px-2 py-1">
+            <CopyIcon className="size-3" />
+            <span className="text-[10px] text-zinc-400">
+              {value.slice(0, 6)}...{value.slice(-6)}
+            </span>
+          </div>
         </div>
       ))}
     </div>
